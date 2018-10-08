0903f6800488438b6de526f8cc4e393e9c95a01c
Google Research Team <no-reply@google.com>
2018-10-08 10:30:35 -0700

    Code to reproduce the experiments in Doubly Reparameterized Gradient Estimators for Monte Carlo Objectives. George Tucker, Dieterich Lawson, Shixiang Gu, Chris J. Maddison.
    PiperOrigin-RevId: 216208961

---
+++ dreg_estimators/main_loop.py
@@ -0,0 +1,334 @@
+# coding=utf-8
+# Copyright 2018 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Training/eval loop for DReGs."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import os
+import random
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+from dreg_estimators import model
+from dreg_estimators import utils
+from tensorflow.python.training import summary_io
+
+tfd = tfp.distributions
+flags = tf.flags
+
+flags.DEFINE_enum("estimator", "iwae", [
+    "iwae", "rws", "stl", "dreg", "dreg-cv", "rws-dreg", "rws-dreg-norm",
+    "dreg-norm", "jk", "jk-dreg", "dreg-alpha"
+], "Estimator type to use.")
+flags.DEFINE_float("alpha", 0.9, "Weighting for DReG(alpha)")
+flags.DEFINE_integer("batch_size", 32, "The batch size.")
+flags.DEFINE_integer("num_samples", 64, "The numer of samples to use.")
+flags.DEFINE_integer("latent_dim", 50, "The dimension of the VAE latent space.")
+flags.DEFINE_float("learning_rate", 3e-4, "The learning rate for ADAM.")
+flags.DEFINE_integer("max_steps", int(5e6), "The number of steps to train for.")
+flags.DEFINE_integer("summarize_every", 100,
+                     "Number of steps between summaries.")
+flags.DEFINE_string("logdir", "/tmp/dreg",
+                    "The directory to put summaries and checkpoints.")
+flags.DEFINE_bool("bias_check", False,
+                  "Run a bias check instead of training the model.")
+flags.DEFINE_string(
+    "initial_checkpoint_dir", None,
+    ("Initial checkpoint directory to start from. This also disables model ",
+     "training. Only the inference network is trained.")
+)
+flags.DEFINE_integer(
+    "run", 0,
+    ("A number to distinguish which run this is. This allows us to run ",
+     "multiple trials with the same params.")
+)
+flags.DEFINE_string(
+    "var_calc", None,
+    ("Comma separated list of estimators to calculate the variance of on this",
+     "trajectory")
+)
+flags.DEFINE_enum("dataset", "mnist", [
+    "mnist",
+    "struct_mnist",
+    "omniglot",
+], "Dataset to use.")
+flags.DEFINE_bool("image_summary", False, "Create visualizations")
+
+FLAGS = flags.FLAGS
+
+
+def create_logging_hook(metrics):
+
+  def summary_formatter(d):
+    return ", ".join(
+        ["%s: %g" % (key, float(value)) for key, value in sorted(d.items())])
+
+  logging_hook = tf.train.LoggingTensorHook(
+      metrics, formatter=summary_formatter, every_n_iter=FLAGS.summarize_every)
+  return logging_hook
+
+
+def main(unused_argv):
+  proposal_hidden_dims = [200, 200]
+  likelihood_hidden_dims = [200, 200]
+
+  with tf.Graph().as_default():
+    alpha = tf.Variable(0.0, name="alpha_cv")
+    beta = tf.Variable(0.0, name="beta_cv")
+    gamma = tf.Variable(0.0, name="gamma_cv")
+    delta = tf.Variable(0.0, name="delta_cv")
+    tf.add_to_collection("CONTROL_VARIATES", alpha)
+    tf.add_to_collection("CONTROL_VARIATES", beta)
+    tf.add_to_collection("CONTROL_VARIATES", gamma)
+    tf.add_to_collection("CONTROL_VARIATES", delta)
+
+    if FLAGS.dataset in ["mnist", "struct_mnist"]:
+      train_xs, valid_xs, test_xs = utils.load_mnist()
+    elif FLAGS.dataset == "omniglot":
+      train_xs, valid_xs, test_xs = utils.load_omniglot()
+
+    # Compute bias initializer on the training set
+    mean_xs = np.mean(train_xs, axis=0)
+    clipped_mean_xs = np.clip(mean_xs, 1e-3, 1 - 1e-3)
+    bias_init = np.log(clipped_mean_xs / (1 - clipped_mean_xs))
+
+    # Placeholder for input mnist digits.
+    observations_ph = tf.placeholder("float32", [None, 784])
+
+    if FLAGS.dataset == "struct_mnist":
+      context_mean_xs = np.split(mean_xs, 2, 0)[0]
+      prior = model.ConditionalNormal(
+          FLAGS.latent_dim,
+          likelihood_hidden_dims,
+          mean_center=context_mean_xs,
+          hidden_activation_fn=tf.nn.tanh)
+      proposal = model.ConditionalNormal(
+          FLAGS.latent_dim,
+          proposal_hidden_dims,
+          mean_center=mean_xs,
+          hidden_activation_fn=tf.nn.tanh)
+      likelihood = model.ConditionalBernoulli(
+          784 // 2,
+          likelihood_hidden_dims,
+          bias_init=np.split(bias_init, 2, 0)[1],
+          hidden_activation_fn=tf.nn.tanh)
+      observations, contexts = tf.split(
+          observations_ph, num_or_size_splits=2, axis=1)
+      # pylint: disable=g-long-lambda
+      get_model_params = (lambda: likelihood.fcnet.get_variables() +
+                          prior.fcnet.get_variables())
+      # pylint: enable=g-long-lambda
+    else:
+      # prior is Normal(0, 1)
+      prior_loc = tf.zeros([FLAGS.latent_dim], dtype=tf.float32)
+      prior_scale = tf.ones([FLAGS.latent_dim], dtype=tf.float32)
+      prior = lambda _: tfd.Normal(loc=prior_loc, scale=prior_scale)
+      proposal = model.ConditionalNormal(
+          FLAGS.latent_dim,
+          proposal_hidden_dims,
+          mean_center=mean_xs,
+          hidden_activation_fn=tf.nn.tanh)
+      likelihood = model.ConditionalBernoulli(
+          784,
+          likelihood_hidden_dims,
+          bias_init=bias_init,
+          hidden_activation_fn=tf.nn.tanh)
+      observations, contexts = observations_ph, None
+      get_model_params = lambda: likelihood.fcnet.get_variables()  # pylint: disable=unnecessary-lambda
+
+    # Compute the lower bound and the loss
+    estimators = model.iwae(
+        prior,
+        likelihood,
+        proposal,
+        observations,
+        FLAGS.num_samples, [alpha, beta, gamma, delta],
+        contexts=contexts)
+
+    log_p_hat, neg_model_loss, neg_inference_loss = estimators[FLAGS.estimator]
+    model_loss = -tf.reduce_mean(neg_model_loss)
+    inference_loss = -tf.reduce_mean(neg_inference_loss)
+    log_p_hat_mean = tf.reduce_mean(log_p_hat)
+
+    model_params = get_model_params()
+    inference_network_params = proposal.fcnet.get_variables()
+
+    # Compute and apply the gradients, summarizing the gradient variance.
+    global_step = tf.train.get_or_create_global_step()
+    opt = tf.train.AdamOptimizer(FLAGS.learning_rate)
+    model_grads = opt.compute_gradients(model_loss, var_list=model_params)
+    inference_grads = opt.compute_gradients(
+        inference_loss, var_list=inference_network_params)
+
+    # If we're using control variates, add gradients for these too.
+    if "cv" in FLAGS.estimator:
+      vectorized_inference_grads = tf.concat(
+          [tf.reshape(g, [-1]) for g, _ in inference_grads if g is not None],
+          axis=0)
+      cv_grads = opt.compute_gradients(
+          tf.reduce_mean(tf.square(vectorized_inference_grads)),
+          var_list=tf.get_collection("CONTROL_VARIATES"))
+      tf.summary.scalar("alpha", alpha)
+      tf.summary.scalar("beta", beta)
+      tf.summary.scalar("gamma", gamma)
+      tf.summary.scalar("delta", delta)
+    else:
+      cv_grads = []
+
+    if FLAGS.initial_checkpoint_dir:
+      # Just train the inference network from the initial checkpoint
+      train_op = opt.apply_gradients(
+          inference_grads + cv_grads, global_step=global_step)
+      model_grad_variance = tf.constant(0.)
+      inference_grad_variance = tf.constant(0.)
+      inference_grad_snr_sq = tf.constant(0.)
+    else:
+      grads = model_grads + inference_grads + cv_grads
+      model_ema_op, model_grad_variance, _ = (
+          utils.summarize_grads(model_grads))
+      inference_ema_op, inference_grad_variance, inference_grad_snr_sq = (
+          utils.summarize_grads(inference_grads))
+
+      ema_ops = [model_ema_op, inference_ema_op]
+      if FLAGS.var_calc is not None:
+        var_calc = FLAGS.var_calc.split(",")
+        for estimator in var_calc:
+          var_calc_inference_grads = opt.compute_gradients(
+              -tf.reduce_mean(estimators[estimator][-1]),
+              var_list=inference_network_params)
+          (var_calc_inference_ema_op, var_calc_inference_grad_variance,
+           var_calc_inference_grad_snr_sq
+          ) = utils.summarize_grads(var_calc_inference_grads)
+          ema_ops.append(var_calc_inference_ema_op)
+
+          # Add summaries
+          tf.summary.scalar("inference_grad_variance/%s" % estimator,
+                            var_calc_inference_grad_variance)
+          tf.summary.scalar("inference_grad_snr_sq/%s" % estimator,
+                            var_calc_inference_grad_snr_sq)
+
+      with tf.control_dependencies(ema_ops):
+        train_op = opt.apply_gradients(grads, global_step=global_step)
+
+      tf.summary.scalar("model_grad_variance", model_grad_variance)
+      tf.summary.scalar("inference_grad_variance/%s" % FLAGS.estimator,
+                        inference_grad_variance)
+      tf.summary.scalar("inference_grad_snr_sq/%s" % FLAGS.estimator,
+                        inference_grad_snr_sq)
+    tf.summary.scalar("log_p_hat/train", log_p_hat_mean)
+
+    # Define an op to compute the paired t statistic (for bias checking)
+    iwae_inference_grads = opt.compute_gradients(
+        -log_p_hat_mean, var_list=inference_network_params)
+    n = 0.
+    t_stat = 0.
+    for (g_a, _), (g_b, _) in zip(inference_grads, iwae_inference_grads):
+      n += tf.to_float(tf.size(g_a))
+      t_stat += tf.reduce_sum(g_a - g_b)
+    t_stat /= n
+
+    exp_name = "%s.lr-%g.n_samples-%d.alpha-%g.dataset-%s.run-%d" % (
+        FLAGS.estimator, FLAGS.learning_rate, FLAGS.num_samples, FLAGS.alpha,
+        FLAGS.dataset, FLAGS.run)
+    checkpoint_dir = os.path.join(FLAGS.logdir, exp_name)
+    if FLAGS.initial_checkpoint_dir and not tf.gfile.Exists(checkpoint_dir):
+      tf.gfile.MakeDirs(checkpoint_dir)
+      f = "checkpoint"
+      tf.gfile.Copy(
+          os.path.join(FLAGS.initial_checkpoint_dir, f),
+          os.path.join(checkpoint_dir, f))
+
+    with tf.train.MonitoredTrainingSession(
+        is_chief=True,
+        hooks=[
+            create_logging_hook({
+                "Step": global_step,
+                "log_p_hat": log_p_hat_mean,
+                "model_grad_variance": model_grad_variance,
+                "infer_grad_varaince": inference_grad_variance,
+                "infer_grad_snr_sq": inference_grad_snr_sq,
+                "alpha": alpha,
+                "beta": beta,
+            })
+        ],
+        checkpoint_dir=checkpoint_dir,
+        save_checkpoint_secs=120,
+        save_summaries_steps=FLAGS.summarize_every,
+        log_step_count_steps=FLAGS.summarize_every * 10) as sess:
+      writer = summary_io.SummaryWriterCache.get(checkpoint_dir)
+      t_stats = []
+      cur_step = -1
+      indices = list(range(train_xs.shape[0]))
+      n_epoch = 0
+
+      def run_eval(cur_step, split="valid", eval_batch_size=256):
+        """Run evaluation on a datasplit."""
+        if split == "valid":
+          eval_dataset = valid_xs
+        elif split == "test":
+          eval_dataset = test_xs
+
+        log_p_hat_vals = []
+        for i in range(0, eval_dataset.shape[0], eval_batch_size):
+          batch_xs = utils.binarize_batch_xs(
+              eval_dataset[i:(i + eval_batch_size)])
+          log_p_hat_vals.append(
+              sess.run(log_p_hat_mean, feed_dict={observations_ph: batch_xs}))
+
+        summary = tf.Summary(value=[
+            tf.Summary.Value(
+                tag="log_p_hat/%s" % split,
+                simple_value=np.mean(log_p_hat_vals))
+        ])
+        writer.add_summary(summary, cur_step)
+        print("log_p_hat/%s: %g" % (split, np.mean(log_p_hat_vals)))
+
+      while cur_step < FLAGS.max_steps and not sess.should_stop():
+        n_epoch += 1
+        random.shuffle(indices)
+
+        for i in range(0, train_xs.shape[0], FLAGS.batch_size):
+          if sess.should_stop() or cur_step > FLAGS.max_steps:
+            break
+
+          # Get a batch, then dynamically binarize
+          ns = indices[i:i + FLAGS.batch_size]
+          batch_xs = utils.binarize_batch_xs(train_xs[ns])
+
+          if FLAGS.bias_check and cur_step > 1000:
+            t_stat_val, = sess.run([t_stat],
+                                   feed_dict={observations_ph: batch_xs})
+            t_stats.append(t_stat_val)
+            aggregate_t_stat = (
+                np.mean(t_stats) /
+                (np.std(t_stats, ddof=1) / np.sqrt(len(t_stats))))
+            print(
+                len(t_stats), np.mean(t_stats), np.std(t_stats, ddof=1),
+                aggregate_t_stat)
+          else:
+            _, cur_step = sess.run([train_op, global_step],
+                                   feed_dict={observations_ph: batch_xs})
+
+        if n_epoch % 10 == 0:
+          # Run a validation step and test step
+          run_eval(cur_step, "valid")
+          run_eval(cur_step, "test")
+
+
+if __name__ == "__main__":
+  tf.app.run(main)
---
+++ dreg_estimators/model.py
@@ -0,0 +1,346 @@
+# coding=utf-8
+# Copyright 2018 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Basic IWAE setup with DReGs estimators."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import sonnet as snt
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+tfd = tfp.distributions
+FLAGS = tf.flags.FLAGS
+
+DEFAULT_INITIALIZERS = {
+    "w": tf.contrib.layers.xavier_initializer(),
+    "b": tf.zeros_initializer()
+}
+
+
+class ConditionalBernoulli(object):
+  """A Bernoulli distribution conditioned on Tensor inputs via a fc net."""
+
+  def __init__(self,
+               size,
+               hidden_layer_sizes,
+               hidden_activation_fn=tf.nn.tanh,
+               initializers=None,
+               bias_init=0.0,
+               name="conditional_bernoulli"):
+    """Creates a conditional Bernoulli distribution.
+
+    Args:
+      size: The dimension of the random variable.
+      hidden_layer_sizes: The sizes of the hidden layers of the fully connected
+        network used to condition the distribution on the inputs.
+      hidden_activation_fn: The activation function to use on the hidden layers
+        of the fully connected network.
+      initializers: The variable intiializers to use for the fully connected
+        network. The network is implemented using snt.nets.MLP so it must be a
+        dictionary mapping the keys 'w' and 'b' to the initializers for the
+        weights and biases. Defaults to xavier for the weights and zeros for the
+        biases when initializers is None.
+      bias_init: A scalar or vector Tensor that is added to the output of the
+        fully-connected network that parameterizes the mean of this
+        distribution.
+      name: The name of this distribution, used for sonnet scoping.
+    """
+    self.bias_init = bias_init
+    self.size = size
+    if initializers is None:
+      initializers = DEFAULT_INITIALIZERS
+    self.fcnet = snt.nets.MLP(
+        output_sizes=hidden_layer_sizes + [size],
+        activation=hidden_activation_fn,
+        initializers=initializers,
+        activate_final=False,
+        use_bias=True,
+        name=name + "_fcnet")
+
+  def condition(self, tensor_list):
+    """Computes the p parameter of the Bernoulli distribution."""
+    # Remove None's from tensor_list
+    tensor_list = [t for t in tensor_list if t is not None]
+    concatted_inputs = tf.concat(tensor_list, axis=-1)
+    input_dim = concatted_inputs.get_shape().as_list()[-1]
+    raw_input_shape = tf.shape(concatted_inputs)[:-1]
+    fcnet_input_shape = [tf.reduce_prod(raw_input_shape), input_dim]
+    fcnet_inputs = tf.reshape(concatted_inputs, fcnet_input_shape)
+    outs = self.fcnet(fcnet_inputs) + self.bias_init
+    # Reshape outputs to the original shape.
+    output_size = tf.concat([raw_input_shape, [self.size]], axis=0)
+    return tf.reshape(outs, output_size)
+
+  def __call__(self, *args, **kwargs):
+    p = self.condition(args)
+    if kwargs.get("stop_gradient", False):
+      p = tf.stop_gradient(p)
+    return tfd.Bernoulli(logits=p)
+
+
+class ConditionalNormal(object):
+  """A Normal distribution conditioned on Tensor inputs via a fc network."""
+
+  def __init__(self,
+               size,
+               hidden_layer_sizes,
+               mean_center=None,
+               sigma_min=0.0,
+               raw_sigma_bias=0.25,
+               hidden_activation_fn=tf.nn.tanh,
+               initializers=None,
+               name="conditional_normal"):
+    """Creates a conditional Normal distribution.
+
+    Args:
+      size: The dimension of the random variable.
+      hidden_layer_sizes: The sizes of the hidden layers of the fully connected
+        network used to condition the distribution on the inputs.
+      mean_center: Optionally, mean center the data using this Tensor as the
+        mean.
+      sigma_min: The minimum standard deviation allowed, a scalar.
+      raw_sigma_bias: A scalar that is added to the raw standard deviation
+        output from the fully connected network. Set to 0.25 by default to
+        prevent standard deviations close to 0.
+      hidden_activation_fn: The activation function to use on the hidden layers
+        of the fully connected network.
+      initializers: The variable intitializers to use for the fully connected
+        network. The network is implemented using snt.nets.MLP so it must be a
+        dictionary mapping the keys 'w' and 'b' to the initializers for the
+        weights and biases. Defaults to xavier for the weights and zeros for the
+        biases when initializers is None.
+      name: The name of this distribution, used for sonnet scoping.
+    """
+    self.sigma_min = sigma_min
+    self.raw_sigma_bias = raw_sigma_bias
+    self.name = name
+    self.mean_center = mean_center
+    if initializers is None:
+      initializers = DEFAULT_INITIALIZERS
+    self.fcnet = snt.nets.MLP(
+        output_sizes=hidden_layer_sizes + [2 * size],
+        activation=hidden_activation_fn,
+        initializers=initializers,
+        activate_final=False,
+        use_bias=True,
+        name=name + "_fcnet")
+
+  def condition(self, tensor_list):
+    """Computes the parameters of a normal distribution based on the inputs."""
+    # Remove None's from tensor_list
+    tensor_list = [t for t in tensor_list if t is not None]
+    inputs = tf.concat(tensor_list, axis=1)
+    if self.mean_center is not None:
+      inputs -= self.mean_center
+    outs = self.fcnet(inputs)
+    mu, sigma = tf.split(outs, 2, axis=1)
+    sigma = tf.maximum(
+        tf.nn.softplus(sigma + self.raw_sigma_bias), self.sigma_min)
+    return mu, sigma
+
+  def __call__(self, *args, **kwargs):
+    """Creates a normal distribution conditioned on the inputs."""
+    mu, sigma = self.condition(args)
+
+    # Optional stop_gradient argument stops the parameters of the distribution.
+    # TODO(gjt): This only works for 1 latent layer networks.
+    if kwargs.get("stop_gradient", False):
+      mu = tf.stop_gradient(mu)
+      sigma = tf.stop_gradient(sigma)
+    return tfd.Normal(loc=mu, scale=sigma)
+
+
+def iwae(p_z,
+         p_x_given_z,
+         q_z,
+         observations,
+         num_samples,
+         cvs,
+         contexts=None,
+         antithetic=False):
+  """Computes a gradient of the IWAE estimator.
+
+  Args:
+    p_z: The prior. Should be a callable that optionally accepts a conditioning
+      context and returns a tf.distributions.Distribution which has the log_prob
+      and sample methods implemented. The distribution should be over a
+      [batch_size, latent_dim] space.
+    p_x_given_z: The likelihood. Should be a callable that accepts as input a
+      tensor of shape [num_samples, batch_size, latent_size + context_size] and
+      returns a tfd.Distribution over a [num_samples, batch_size, data_dim]
+      space.
+    q_z: The proposal, should be a callable which accepts a batch of
+      observations of shape [batch_size, data_dim] and returns a distribution
+      over [batch_size, latent_dim].
+    observations: A float Tensor of shape [batch_size, data_dim] containing the
+      observations.
+    num_samples: The number of samples for the IWAE estimator.
+    cvs: Control variate variables.
+    contexts: A float Tensor of shape [batch_size, context_dim] containing the
+      contexts. (Optionally, none)
+    antithetic: Whether to use antithetic sampling.
+
+  Returns:
+    estimators: Dictionary of tuples (objective, neg_model_loss,
+      neg_inference_network_loss).
+  """
+  alpha, beta, gamma, delta = cvs
+  batch_size = tf.shape(observations)[0]
+  proposal = q_z(observations, contexts, stop_gradient=False)
+  # [num_samples, batch_size, latent_size]
+
+  # If antithetic sampling, draw half of the samples and use the antithetics
+  # for the other half.
+  if antithetic:
+    z_pos = proposal.sample(sample_shape=[num_samples // 2])
+    z_neg = 2 * proposal.loc - z_pos
+    z = tf.concat((z_pos, z_neg), axis=0)
+  else:
+    z = proposal.sample(sample_shape=[num_samples])
+
+  tiled_contexts = None
+  if contexts is not None:
+    tiled_contexts = tf.tile(tf.expand_dims(contexts, 0), [num_samples, 1, 1])
+  likelihood = p_x_given_z(z, tiled_contexts)
+  # Before reduce_sum is [num_samples, batch_size, latent_dim].
+  # Sum over the latent dim.
+  log_q_z = tf.reduce_sum(proposal.log_prob(z), axis=-1)
+  # Before reduce_sum is  [num_samples, batch_size, latent_dim].
+  # Sum over latent dim.
+  prior = p_z(contexts)
+  log_p_z = tf.reduce_sum(prior.log_prob(z), axis=-1)
+  # Before reduce_sum is [num_samples, batch_size, data_dim]
+  log_p_x_given_z = tf.reduce_sum(likelihood.log_prob(observations), axis=-1)
+
+  log_weights = log_p_z + log_p_x_given_z - log_q_z
+  log_sum_weight = tf.reduce_logsumexp(log_weights, axis=0)
+  log_avg_weight = log_sum_weight - tf.log(tf.to_float(num_samples))
+  normalized_weights = tf.stop_gradient(tf.nn.softmax(log_weights, axis=0))
+
+  if FLAGS.image_summary:
+    best_index = tf.to_int32(tf.argmax(normalized_weights, axis=0))
+    indices = tf.stack((best_index, tf.range(0, batch_size)), axis=-1)
+    best_images = tf.nn.sigmoid(tf.gather_nd(likelihood.logits, indices))
+
+    if FLAGS.dataset == "struct_mnist":
+      tf.summary.image("bottom_half",
+                       tf.reshape(best_images, [batch_size, -1, 28, 1]))
+    else:
+      tf.summary.image("output", tf.reshape(best_images,
+                                            [batch_size, -1, 28, 1]))
+    tf.summary.image("input", tf.reshape(observations, [batch_size, -1, 28, 1]))
+
+  # Compute gradient estimators
+  model_loss = log_avg_weight
+  estimators = {}
+
+  estimators["iwae"] = (log_avg_weight, log_avg_weight, log_avg_weight)
+
+  stopped_z_log_q_z = tf.reduce_sum(
+      proposal.log_prob(tf.stop_gradient(z)), axis=-1)
+  estimators["rws"] = (log_avg_weight, model_loss,
+                       tf.reduce_sum(
+                           normalized_weights * stopped_z_log_q_z, axis=0))
+
+  # Doubly reparameterized
+  stopped_proposal = q_z(observations, contexts, stop_gradient=True)
+  stopped_log_q_z = tf.reduce_sum(stopped_proposal.log_prob(z), axis=-1)
+  stopped_log_weights = log_p_z + log_p_x_given_z - stopped_log_q_z
+  sq_normalized_weights = tf.square(normalized_weights)
+
+  estimators["stl"] = (log_avg_weight, model_loss,
+                       tf.reduce_sum(
+                           normalized_weights * stopped_log_weights, axis=0))
+  estimators["dreg"] = (log_avg_weight, model_loss,
+                        tf.reduce_sum(
+                            sq_normalized_weights * stopped_log_weights,
+                            axis=0))
+  estimators["rws-dreg"] = (
+      log_avg_weight, model_loss,
+      tf.reduce_sum(
+          (normalized_weights - sq_normalized_weights) * stopped_log_weights,
+          axis=0))
+
+  # Add normed versions
+  normalized_sq_normalized_weights = (
+      sq_normalized_weights / tf.reduce_sum(
+          sq_normalized_weights, axis=0, keepdims=True))
+  estimators["dreg-norm"] = (
+      log_avg_weight, model_loss,
+      tf.reduce_sum(
+          normalized_sq_normalized_weights * stopped_log_weights, axis=0))
+
+  rws_dregs_weights = normalized_weights - sq_normalized_weights
+  normalized_rws_dregs_weights = rws_dregs_weights / tf.reduce_sum(
+      rws_dregs_weights, axis=0, keepdims=True)
+  estimators["rws-dreg-norm"] = (
+      log_avg_weight, model_loss,
+      tf.reduce_sum(normalized_rws_dregs_weights * stopped_log_weights, axis=0))
+
+  estimators["dreg-alpha"] = (log_avg_weight, model_loss,
+                              (1 - FLAGS.alpha) * estimators["dreg"][-1] +
+                              FLAGS.alpha * estimators["rws-dreg"][-1])
+
+  # Jackknife
+  loo_log_weights = tf.tile(
+      tf.expand_dims(tf.transpose(log_weights), -1), [1, 1, num_samples])
+  loo_log_weights = tf.matrix_set_diag(
+      loo_log_weights, -np.inf * tf.ones([batch_size, num_samples]))
+  loo_log_avg_weight = tf.reduce_mean(
+      tf.reduce_logsumexp(loo_log_weights, axis=1) - tf.log(
+          tf.to_float(num_samples - 1)),
+      axis=-1)
+  jk_model_loss = num_samples * log_avg_weight - (
+      num_samples - 1) * loo_log_avg_weight
+
+  estimators["jk"] = (jk_model_loss, jk_model_loss, jk_model_loss)
+
+  # Compute JK w/ DReG for the inference network
+  loo_normalized_weights = tf.reduce_mean(
+      tf.square(tf.stop_gradient(tf.nn.softmax(loo_log_weights, axis=1))),
+      axis=-1)
+  estimators["jk-dreg"] = (
+      jk_model_loss, jk_model_loss, num_samples * tf.reduce_sum(
+          sq_normalized_weights * stopped_log_weights, axis=0) -
+      (num_samples - 1) * tf.reduce_sum(
+          tf.transpose(loo_normalized_weights) * stopped_log_weights, axis=0))
+
+  # Compute control variates
+  loo_baseline = tf.expand_dims(tf.transpose(log_weights), -1)
+  loo_baseline = tf.tile(loo_baseline, [1, 1, num_samples])
+  loo_baseline = tf.matrix_set_diag(
+      loo_baseline, -np.inf * tf.ones_like(tf.transpose(log_weights)))
+  loo_baseline = tf.reduce_logsumexp(loo_baseline, axis=1)
+  loo_baseline = tf.transpose(loo_baseline)
+
+  learning_signal = tf.stop_gradient(tf.expand_dims(
+      log_avg_weight, 0)) - (1 - gamma) * tf.stop_gradient(loo_baseline)
+  vimco = tf.reduce_sum(learning_signal * stopped_z_log_q_z, axis=0)
+
+  first_part = alpha * vimco + (1 - alpha) * tf.reduce_sum(
+      normalized_weights * stopped_log_weights, axis=0)
+  second_part = ((1 - beta) * (tf.reduce_sum(
+      ((1 - delta) / tf.to_float(num_samples) - normalized_weights) *
+      stopped_z_log_q_z,
+      axis=0)) + beta * tf.reduce_sum(
+          (sq_normalized_weights - normalized_weights) * stopped_log_weights,
+          axis=0))
+  estimators["dreg-cv"] = (log_avg_weight, model_loss, first_part + second_part)
+
+  return estimators
