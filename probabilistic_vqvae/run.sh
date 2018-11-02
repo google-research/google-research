290d90ce2847c7abf5f9b4218ca1f0903b1df226
Andrew Chen <chenandrew@google.com>
2018-11-01 17:05:35 -0700

    Lower test parameters to run on travis
    PiperOrigin-RevId: 219725054

--- probabilistic_vqvae/run.sh
+++ probabilistic_vqvae/run.sh
@@ -9,4 +9,6 @@ pip install tf-nightly
 pip install tfp-nightly
 pip install -r probabilistic_vqvae/requirements.txt
 python -m probabilistic_vqvae.mnist_experiments \
-          --max_steps=500 --latent_size 2 --code_size 4 --base_depth 8 --batch_size 16
+          --max_steps=500 \
+          --latent_size 2 --num_codes 2 --code_size 4 --base_depth 2 --batch_size 4 \
+          --nouse_autoregressive_prior
