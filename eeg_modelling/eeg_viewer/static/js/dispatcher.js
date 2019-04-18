de13f1c1bfd224cb3f150485d78f8f70cd7fdee6
Pablo Pino <pdpino@google.com>
2019-04-18 11:42:11 -0700

    [eeg_modelling] Accept or reject similar patterns
    PiperOrigin-RevId: 244228233

--- eeg_modelling/eeg_viewer/static/js/dispatcher.js
+++ eeg_modelling/eeg_viewer/static/js/dispatcher.js
@@ -38,6 +38,8 @@ const ActionType = {
   SEARCH_SIMILAR_REQUEST: 'searchSimilarRequest',
   SEARCH_SIMILAR_RESPONSE_OK: 'searchSimilarResponseOk',
   SEARCH_SIMILAR_RESPONSE_ERROR: 'searchSimilarResponseError',
+  SIMILAR_PATTERN_ACCEPT: 'similarPatternAccept',
+  SIMILAR_PATTERN_REJECT: 'similarPatternReject',
   TOOL_BAR_GRIDLINES: 'toolBarGridlines',
   TOOL_BAR_HIGH_CUT: 'toolBarHighCut',
   TOOL_BAR_LOW_CUT: 'toolBarLowCut',
