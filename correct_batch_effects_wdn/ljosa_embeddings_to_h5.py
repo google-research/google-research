264a020f4de7343fbc5e2116837c483ae639bce3
Gil Tabak <tabakg@google.com>
2019-05-15 16:46:45 -0700

    Finish updating to Python 3.
    PiperOrigin-RevId: 248431677

--- correct_batch_effects_wdn/ljosa_embeddings_to_h5.py
+++ correct_batch_effects_wdn/ljosa_embeddings_to_h5.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Lint as: python2, python3
 """Convert Ljosa embeddings to h5 dataframe."""
 
 from __future__ import absolute_import
