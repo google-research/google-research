bce209ecb5572e24bc05b2c31d7df76e196b1b93
cclauss <cclauss@me.com>
2019-06-28 14:48:57 +0200

    ur"strings" are syntax errors in Python 3

--- meta_reward_learning/semantic_parsing/table/wtq/evaluator.py
+++ meta_reward_learning/semantic_parsing/table/wtq/evaluator.py
@@ -87,25 +87,25 @@ def normalize(x):
       c for c in unicodedata.normalize('NFKD', x)
       if unicodedata.category(c) != 'Mn')
   # Normalize quotes and dashes
-  x = re.sub(ur'[‘’´`]', "'", x)
-  x = re.sub(ur'[“”]', '"', x)
-  x = re.sub(ur'[‐‑‒–—−]', '-', x)
+  x = re.sub(u'[‘’´`]', "'", x)
+  x = re.sub(u'[“”]', '"', x)
+  x = re.sub(u'[‐‑‒–—−]', '-', x)
   while True:
     old_x = x
     # Remove citations
-    x = re.sub(ur'((?<!^)\[[^\]]*\]|\[\d+\]|[•♦†‡*#+])*$', '',
+    x = re.sub(u'((?<!^)\[[^\]]*\]|\[\d+\]|[•♦†‡*#+])*$', '',
                x.strip())
     # Remove details in parenthesis
-    x = re.sub(ur'(?<!^)( \([^)]*\))*$', '', x.strip())
+    x = re.sub(u'(?<!^)( \([^)]*\))*$', '', x.strip())
     # Remove outermost quotation mark
-    x = re.sub(ur'^"([^"]*)"$', r'\1', x.strip())
+    x = re.sub(u'^"([^"]*)"$', r'\1', x.strip())
     if x == old_x:
       break
   # Remove final '.'
   if x and x[-1] == '.':
     x = x[:-1]
   # Collapse whitespaces and convert to lower case
-  x = re.sub(ur'\s+', ' ', x, flags=re.U).lower().strip()
+  x = re.sub(u'\s+', ' ', x, flags=re.U).lower().strip()
   return x
 
 
