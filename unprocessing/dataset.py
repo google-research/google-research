c80499ac39616f317e9c48bcb5435788538e2309
Jon Barron <barron@google.com>
2019-07-01 14:56:41 -0700

    Add evaluation on Darmstadt dataset for "Unprocessing Images for Learned Raw Denoising", CVPR 2019.
    PiperOrigin-RevId: 256039110

--- unprocessing/dataset.py
+++ unprocessing/dataset.py
@@ -21,7 +21,6 @@ http://timothybrooks.com/tech/unprocessing
 
 from __future__ import absolute_import
 from __future__ import division
-
 from __future__ import print_function
 
 import tensorflow as tf
