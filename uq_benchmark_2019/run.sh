1166e4ac2af48656a8e7601952b431e6e1157e52
Yaniv Ovadia <yovadia@google.com>
2019-07-15 10:56:17 -0700

    Fix uq_benchmark_2019/run.sh instructions and add criteo tests.
    PiperOrigin-RevId: 258196254

--- uq_benchmark_2019/run.sh
+++ uq_benchmark_2019/run.sh
@@ -16,11 +16,11 @@
 
 # To run:
 # 1. Create a virtual environment:
-#   virtualenv myenv && ./myenv/bin/activate
+#   virtualenv myenv && source myenv/bin/activate
 # 2. Install dependencies:
 #   pip install -r uq_benchmark_2019/requirements.txt
 # 3. Run tests:
-#   python -m uq_benchmark_2019/run.sh
+#   bash uq_benchmark_2019/run.sh
 
 set -e  # exit on error
 
@@ -31,6 +31,9 @@ fi
 
 python -m uq_benchmark_2019.array_utils_test
 python -m uq_benchmark_2019.uq_utils_test
+python -m uq_benchmark_2019.calibration_lib_test
 python -m uq_benchmark_2019.mnist.experiment_test
 python -m uq_benchmark_2019.cifar.data_lib_test
 python -m uq_benchmark_2019.cifar.end_to_end_test
+python -m uq_benchmark_2019.criteo.data_lib_test --criteo_dummy_path_for_test=uq_benchmark_2019/criteo/criteo_testdata.tfr
+python -m uq_benchmark_2019.criteo.run_train_test --criteo_dummy_path_for_test=uq_benchmark_2019/criteo/criteo_testdata.tfr
