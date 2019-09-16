078a4ed2c57ce15010f2e669ee00aa4384d8a10a
Sercan Arik <soarik@google.com>
2019-09-16 16:04:06 -0700

    Tabnet implementation example for Forest Covertype dataset.
    PiperOrigin-RevId: 269442662

---
+++ tabnet/experiment_covertype.py
@@ -0,0 +1,202 @@
+# coding=utf-8
+# Copyright 2019 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Experiment to train and evaluate the TabNet model on Forest Covertype."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import os
+from absl import app
+import data_helper_covertype
+import numpy as np
+import tabnet_model
+import tensorflow as tf
+
+# Run Tensorflow on GPU 0
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+# Fix random seeds
+tf.set_random_seed(1)
+np.random.seed(1)
+
+
+def main(unused_argv):
+
+  # Load training and eval data.
+
+  train_file = "data/train.csv"
+  val_file = "data/val.csv"
+  test_file = "data/test.csv"
+
+  # Define the TabNet model
+  tabnet_forest_covertype = tabnet_model.TabNet(
+      columns=data_helper_covertype.get_columns(),
+      num_features=data_helper_covertype.num_features,
+      feature_dim=128,
+      output_dim=64,
+      num_decision_steps=6,
+      relaxation_factor=1.5,
+      batch_momentum=0.7,
+      virtual_batch_size=512,
+      num_classes=data_helper_covertype.num_classes)
+
+  column_names = sorted(data_helper_covertype.feature_columns)
+  print(
+      "Ordered column names, corresponding to the indexing in Tensorboard visualization"
+  )
+  for fi in range(len(column_names)):
+    print(str(fi) + " : " + column_names[fi])
+
+  # Training parameters
+  max_steps = 1000000
+  display_step = 5000
+  val_step = 10000
+  save_step = 40000
+  init_localearning_rate = 0.02
+  decay_every = 500
+  decay_rate = 0.95
+  batch_size = 16384
+  sparsity_loss_weight = 0.0001
+  gradient_thresh = 2000.0
+
+  # Input sampling
+  train_batch = data_helper_covertype.input_fn(
+      train_file, num_epochs=100000, shuffle=True, batch_size=batch_size)
+  val_batch = data_helper_covertype.input_fn(
+      val_file,
+      num_epochs=10000,
+      shuffle=False,
+      batch_size=data_helper_covertype.n_val_samples)
+  test_batch = data_helper_covertype.input_fn(
+      test_file,
+      num_epochs=10000,
+      shuffle=False,
+      batch_size=data_helper_covertype.n_test_samples)
+
+  train_iter = train_batch.make_initializable_iterator()
+  val_iter = val_batch.make_initializable_iterator()
+  test_iter = test_batch.make_initializable_iterator()
+
+  feature_train_batch, label_train_batch = train_iter.get_next()
+  feature_val_batch, label_val_batch = val_iter.get_next()
+  feature_test_batch, label_test_batch = test_iter.get_next()
+
+  # Define the model and losses
+
+  encoded_train_batch, total_entropy = tabnet_forest_covertype.encoder(
+      feature_train_batch, reuse=False, is_training=True)
+
+  logits_orig_batch, _ = tabnet_forest_covertype.classify(
+      encoded_train_batch, reuse=False)
+
+  softmax_orig_key_op = tf.reduce_mean(
+      tf.nn.sparse_softmax_cross_entropy_with_logits(
+          logits=logits_orig_batch, labels=label_train_batch))
+
+  train_loss_op = softmax_orig_key_op + sparsity_loss_weight * total_entropy
+  tf.summary.scalar("Total loss", train_loss_op)
+
+  # Optimization step
+  global_step = tf.train.get_or_create_global_step()
+  learning_rate = tf.train.exponential_decay(
+      init_localearning_rate,
+      global_step=global_step,
+      decay_steps=decay_every,
+      decay_rate=decay_rate)
+  optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+  update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+  with tf.control_dependencies(update_ops):
+    gvs = optimizer.compute_gradients(train_loss_op)
+    capped_gvs = [(tf.clip_by_value(grad, -gradient_thresh,
+                                    gradient_thresh), var) for grad, var in gvs]
+    train_op = optimizer.apply_gradients(capped_gvs, global_step=global_step)
+
+  # Model evaluation
+
+  # Validation performance
+  encoded_val_batch, _ = tabnet_forest_covertype.encoder(
+      feature_val_batch, reuse=True, is_training=False)
+
+  _, prediction_val = tabnet_forest_covertype.classify(
+      encoded_val_batch, reuse=True)
+
+  predicted_labels = tf.cast(tf.argmax(prediction_val, 1), dtype=tf.int32)
+  val_eq_op = tf.equal(predicted_labels, label_val_batch)
+  val_acc_op = tf.reduce_mean(tf.cast(val_eq_op, dtype=tf.float32))
+  tf.summary.scalar("Val accuracy", val_acc_op)
+
+  # Test performance
+  encoded_test_batch, _ = tabnet_forest_covertype.encoder(
+      feature_test_batch, reuse=True, is_training=False)
+
+  _, prediction_test = tabnet_forest_covertype.classify(
+      encoded_test_batch, reuse=True)
+
+  predicted_labels = tf.cast(tf.argmax(prediction_test, 1), dtype=tf.int32)
+  test_eq_op = tf.equal(predicted_labels, label_test_batch)
+  test_acc_op = tf.reduce_mean(tf.cast(test_eq_op, dtype=tf.float32))
+  tf.summary.scalar("Test accuracy", test_acc_op)
+
+  # Training setup
+  model_name = "tabnet_forest_covertype_model"
+  init = tf.initialize_all_variables()
+  init_local = tf.local_variables_initializer()
+  init_table = tf.tables_initializer(name="Initialize_all_tables")
+  saver = tf.train.Saver()
+  summaries = tf.summary.merge_all()
+
+  with tf.Session() as sess:
+    summary_writer = tf.summary.FileWriter("./tflog/" + model_name, sess.graph)
+
+    sess.run(init)
+    sess.run(init_local)
+    sess.run(init_table)
+    sess.run(train_iter.initializer)
+    sess.run(val_iter.initializer)
+    sess.run(test_iter.initializer)
+
+    for step in range(1, max_steps + 1):
+      if step % display_step == 0:
+        _, train_loss, merged_summary = sess.run(
+            [train_op, train_loss_op, summaries])
+        summary_writer.add_summary(merged_summary, step)
+        print("Step " + str(step) + " , Training Loss = " +
+              "{:.4f}".format(train_loss))
+      else:
+        _ = sess.run(train_op)
+
+      if step % val_step == 0:
+        feed_arr = [
+            vars()["summaries"],
+            vars()["val_acc_op"],
+            vars()["test_acc_op"]
+        ]
+
+        val_arr = sess.run(feed_arr)
+        merged_summary = val_arr[0]
+        val_acc = val_arr[1]
+
+        print("Step " + str(step) + " , Val Accuracy = " +
+              "{:.4f}".format(val_acc))
+        summary_writer.add_summary(merged_summary, step)
+
+      if step % save_step == 0:
+        saver.save(sess, "./checkpoints/" + model_name + ".ckpt")
+
+
+if __name__ == "__main__":
+  app.run(main)
---
+++ tabnet/tabnet_model.py
@@ -0,0 +1,247 @@
+# coding=utf-8
+# Copyright 2019 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""TabNet model."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+
+def glu(act, n_units):
+  """Generalized linear unit nonlinear activation."""
+  return act[:, :n_units] * tf.nn.sigmoid(act[:, n_units:])
+
+
+class TabNet(object):
+  """TabNet model class."""
+
+  def __init__(self,
+               columns,
+               num_features,
+               feature_dim,
+               output_dim,
+               num_decision_steps,
+               relaxation_factor,
+               batch_momentum,
+               virtual_batch_size,
+               num_classes,
+               epsilon=0.00001):
+    """Initializes a TabNet instance.
+
+    Args:
+      columns: The Tensorflow column names for the dataset.
+      num_features: The number of input features (i.e the number of columns for
+        tabular data assuming each feature is represented with 1 dimension).
+      feature_dim: Dimensionality of the hidden representation in feature
+        transformation block. Each layer first maps the representation to a
+        2*feature_dim-dimensional output and half of it is used to determine the
+        nonlinearity of the GLU activation where the other half is used as an
+        input to GLU, and eventually feature_dim-dimensional output is
+        transferred to the next layer.
+      output_dim: Dimensionality of the outputs of each decision step, which is
+        later mapped to the final classification or regression output.
+      num_decision_steps: Number of sequential decision steps.
+      relaxation_factor: Relaxation factor that promotes the reuse of each
+        feature at different decision steps. When it is 1, a feature is enforced
+        to be used only at one decision step and as it increases, more
+        flexibility is provided to use a feature at multiple decision steps.
+      batch_momentum: Momentum in ghost batch normalization.
+      virtual_batch_size: Virtual batch size in ghost batch normalization. The
+        overall batch size should be an integer multiple of virtual_batch_size.
+      num_classes: Number of output classes.
+      epsilon: A small number for numerical stability of the entropy calcations.
+
+    Returns:
+      A TabNet instance.
+    """
+
+    self.columns = columns
+    self.num_features = num_features
+    self.feature_dim = feature_dim
+    self.output_dim = output_dim
+    self.num_decision_steps = num_decision_steps
+    self.relaxation_factor = relaxation_factor
+    self.batch_momentum = batch_momentum
+    self.virtual_batch_size = virtual_batch_size
+    self.num_classes = num_classes
+    self.epsilon = epsilon
+
+  def encoder(self, data, reuse, is_training):
+    """TabNet encoder model."""
+
+    with tf.variable_scope("Encoder", reuse=reuse):
+
+      # Reads and normalizes input features.
+      features = tf.feature_column.input_layer(data, self.columns)
+      features = tf.layers.batch_normalization(
+          features, training=is_training, momentum=self.batch_momentum)
+      batch_size = tf.shape(features)[0]
+
+      # Initializes decision-step dependent variables.
+      output_aggregated = tf.zeros([batch_size, self.output_dim])
+      masked_features = features
+      mask_values = tf.zeros([batch_size, self.num_features])
+      aggregated_mask_values = tf.zeros([batch_size, self.num_features])
+      complemantary_aggregated_mask_values = tf.ones(
+          [batch_size, self.num_features])
+      total_entropy = 0
+
+      if is_training:
+        v_b = self.virtual_batch_size
+      else:
+        v_b = 1
+
+      for ni in range(self.num_decision_steps):
+
+        # Feature transformer with two shared and two decision step dependent
+        # blocks is used below.
+
+        reuse_flag = (ni > 0)
+
+        transform_f1 = tf.layers.dense(
+            masked_features,
+            self.feature_dim * 2,
+            name="Transform_f1",
+            reuse=reuse_flag,
+            use_bias=False)
+        transform_f1 = tf.layers.batch_normalization(
+            transform_f1,
+            training=is_training,
+            momentum=self.batch_momentum,
+            virtual_batch_size=v_b)
+        transform_f1 = glu(transform_f1, self.feature_dim)
+
+        transform_f2 = tf.layers.dense(
+            transform_f1,
+            self.feature_dim * 2,
+            name="Transform_f2",
+            reuse=reuse_flag,
+            use_bias=False)
+        transform_f2 = tf.layers.batch_normalization(
+            transform_f2,
+            training=is_training,
+            momentum=self.batch_momentum,
+            virtual_batch_size=v_b)
+        transform_f2 = (glu(transform_f2, self.feature_dim) +
+                        transform_f1) * np.sqrt(0.5)
+
+        transform_f3 = tf.layers.dense(
+            transform_f2,
+            self.feature_dim * 2,
+            name="Transform_f3" + str(ni),
+            use_bias=False)
+        transform_f3 = tf.layers.batch_normalization(
+            transform_f3,
+            training=is_training,
+            momentum=self.batch_momentum,
+            virtual_batch_size=v_b)
+        transform_f3 = (glu(transform_f3, self.feature_dim) +
+                        transform_f2) * np.sqrt(0.5)
+
+        transform_f4 = tf.layers.dense(
+            transform_f3,
+            self.feature_dim * 2,
+            name="Transform_f4" + str(ni),
+            use_bias=False)
+        transform_f4 = tf.layers.batch_normalization(
+            transform_f4,
+            training=is_training,
+            momentum=self.batch_momentum,
+            virtual_batch_size=v_b)
+        transform_f4 = (glu(transform_f4, self.feature_dim) +
+                        transform_f3) * np.sqrt(0.5)
+
+        if ni > 0:
+
+          decision_out = tf.nn.relu(transform_f4[:, :self.output_dim])
+
+          # Decision aggregation.
+          output_aggregated += decision_out
+
+          # Aggregated masks are used for visualization of the
+          # feature importance attributes.
+          scale_agg = tf.reduce_sum(
+              decision_out, axis=1, keep_dims=True) / (
+                  self.num_decision_steps - 1)
+          aggregated_mask_values += mask_values * scale_agg
+
+        features_for_coef = (transform_f4[:, self.output_dim:])
+
+        if ni < self.num_decision_steps - 1:
+
+          # Determines the feature masks via linear and nonlinear
+          # transformations, taking into account of aggregated feature use.
+          mask_values = tf.layers.dense(
+              features_for_coef,
+              self.num_features,
+              name="Transform_coef" + str(ni),
+              use_bias=False)
+          mask_values = tf.layers.batch_normalization(
+              mask_values,
+              training=is_training,
+              momentum=self.batch_momentum,
+              virtual_batch_size=v_b)
+          mask_values *= complemantary_aggregated_mask_values
+          mask_values = tf.contrib.sparsemax.sparsemax(mask_values)
+
+          # Relaxation factor controls the amount of reuse of features between
+          # different decision blocks and updated with the values of
+          # coefficients.
+          complemantary_aggregated_mask_values *= (
+              self.relaxation_factor - mask_values)
+
+          # Entropy is used to penalize the amount of sparsity in feature
+          # selection.
+          total_entropy += tf.reduce_mean(
+              tf.reduce_sum(
+                  -mask_values * tf.log(mask_values + self.epsilon),
+                  axis=1)) / (
+                      self.num_decision_steps - 1)
+
+          # Feature selection.
+          masked_features = tf.multiply(mask_values, features)
+
+          # Visualization of the feature selection mask at decision step ni
+          tf.summary.image(
+              "Mask for step" + str(ni),
+              tf.expand_dims(tf.expand_dims(mask_values, 0), 3),
+              max_outputs=1)
+
+      # Visualization of the aggregated feature importances
+      tf.summary.image(
+          "Aggregated mask",
+          tf.expand_dims(tf.expand_dims(aggregated_mask_values, 0), 3),
+          max_outputs=1)
+
+      return output_aggregated, total_entropy
+
+  def classify(self, activations, reuse):
+    """TabNet classify block."""
+
+    with tf.variable_scope("Classify", reuse=reuse):
+      logits = tf.layers.dense(activations, self.num_classes, use_bias=False)
+      predictions = tf.nn.softmax(logits)
+      return logits, predictions
+
+  def regress(self, activations, reuse):
+    """TabNet regress block."""
+
+    with tf.variable_scope("Regress", reuse=reuse):
+      predictions = tf.layers.dense(activations, 1)
+      return predictions
