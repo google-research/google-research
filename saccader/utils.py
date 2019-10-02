448bdf329c0ef42c3b6fd31e0c33e2f0190ba49f
Simon Kornblith <skornblith@google.com>
2019-10-02 07:21:54 -0700

    Add pad_to_batch function to utils
    This function pads an arbitrarily structured set of Tensors to a given batch dimension.
    
    PiperOrigin-RevId: 272431842

--- saccader/utils.py
+++ saccader/utils.py
@@ -580,3 +580,73 @@ def metric_fn(logits, labels, mask, **kws):
   return metrics
 
 
+def pad_to_batch(dataset, batch_size):
+  """Pad Tensors to specified batch size.
+
+  Args:
+    dataset: An instance of tf.data.Dataset.
+    batch_size: The number of samples per batch of input requested.
+
+  Returns:
+    An instance of tf.data.Dataset that yields the same Tensors with the same
+    structure as the original padded to batch_size along the leading
+    dimension.
+
+  Raises:
+    ValueError: If the dataset does not comprise any tensors; if a tensor
+      yielded by the dataset has an unknown number of dimensions or is a
+      scalar; or if it can be statically determined that tensors comprising
+      a single dataset element will have different leading dimensions.
+  """
+  def _pad_to_batch(*args):
+    """Given Tensors yielded by a Dataset, pads all to the batch size."""
+    flat_args = tf.nest.flatten(args)
+
+    for tensor in flat_args:
+      if tensor.shape.ndims is None:
+        raise ValueError(
+            "Unknown number of dimensions for tensor %s." % tensor.name)
+      if tensor.shape.ndims == 0:
+        raise ValueError("Tensor %s is a scalar." % tensor.name)
+
+    # This will throw if flat_args is empty. However, as of this writing,
+    # tf.data.Dataset.map will throw first with an internal error, so we do
+    # not check this case explicitly.
+    first_tensor = flat_args[0]
+    first_tensor_shape = tf.shape(first_tensor)
+    first_tensor_batch_size = first_tensor_shape[0]
+    difference = batch_size - first_tensor_batch_size
+
+    for i, tensor in enumerate(flat_args):
+      control_deps = []
+      if i != 0:
+        # Check that leading dimensions of this tensor matches the first,
+        # either statically or dynamically. (If the first dimensions of both
+        # tensors are statically known, the we have to check the static
+        # shapes at graph construction time or else we will never get to the
+        # dynamic assertion.)
+        if (first_tensor.shape[:1].is_fully_defined() and
+            tensor.shape[:1].is_fully_defined()):
+          if first_tensor.shape[0] != tensor.shape[0]:
+            raise ValueError(
+                "Batch size of dataset tensors does not match. %s "
+                "has shape %s, but %s has shape %s" % (
+                    first_tensor.name, first_tensor.shape,
+                    tensor.name, tensor.shape))
+        else:
+          curr_shape = tf.shape(tensor)
+          control_deps = [tf.Assert(
+              tf.equal(curr_shape[0], first_tensor_batch_size),
+              ["Batch size of dataset tensors %s and %s do not match. "
+               "Shapes are" % (tensor.name, first_tensor.name), curr_shape,
+               first_tensor_shape])]
+
+      with tf.control_dependencies(control_deps):
+        # Pad to batch_size along leading dimension.
+        flat_args[i] = tf.pad(
+            tensor, [[0, difference]] + [[0, 0]] * (tensor.shape.ndims - 1))
+      flat_args[i].set_shape([batch_size] + tensor.shape.as_list()[1:])
+
+    return tf.nest.pack_sequence_as(args, flat_args)
+
+  return dataset.map(_pad_to_batch)
