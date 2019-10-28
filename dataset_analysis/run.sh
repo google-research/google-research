eb8e114adbd35b008e570208684a19291235e221
ddemszky <ddemszky@gmail.com>
2019-10-28 14:47:54 -0700

    removed minimal example

--- dataset_analysis/run.sh
+++ dataset_analysis/run.sh
@@ -23,46 +23,5 @@ pip install six
 pip install "tensorflow>=1.11.0,<2.00.0"
 pip install -r dataset_analysis/requirements.txt
 
-echo "Performing training & evaluation..."
-python -m dataset_analysis.bert_classifier \
---do_train=true \
---train_fname=train.tsv \
---dev_fname=dev.tsv \
---multilabel=true \
---data_dir=data/model_input \
---target_file=data/targets.txt \
---vocab_file=pretrained_BERT/cased_L-12_H-768_A-12/vocab.txt \
---bert_config_file=pretrained_BERT/cased_L-12_H-768_A-12/bert_config.json \
---init_checkpoint=pretrained_BERT/cased_L-12_H-768_A-12/bert_model.ckpt \
---max_seq_length=50 \
---train_batch_size=32 \
---learning_rate=2e-5 \
---num_train_epochs=4 \
---sentiment=1 \
---entailment=1e-3 \
---correlation=10 \
---save_checkpoints_steps=1361 \
---output_dir=checkpoints \
---do_lower_case=false \
---alsologtostderr \
---minloglevel=0
-
-echo "Performing prediction..."
-python -m dataset_analysis.bert_classifier \
---do_train=false \
---do_predict=true \
---calculate_metrics=true \
---test_fname=test.tsv \
---multilabel=true \
---data_dir=data/model_input \
---target_file=data/targets.txt \
---vocab_file=pretrained_BERT/cased_L-12_H-768_A-12/vocab.txt \
---bert_config_file=pretrained_BERT/cased_L-12_H-768_A-12/bert_config.json \
---init_checkpoint=pretrained_BERT/cased_L-12_H-768_A-12/bert_model.ckpt \
---max_seq_length=50 \
---output_dir=checkpoints \
---do_lower_case=false \
---alsologtostderr \
---minloglevel=0
-
+echo "This is a test"
 
