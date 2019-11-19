Fixes to small_problem and add proposal variance option for small_problem.py.
PiperOrigin-RevId: 281390400

eim/models/base.py
@@ -250,11 +250,13 @@ class ProbabilisticModel(object):
     pass
 
 
-def get_independent_normal(data_dim):
+def get_independent_normal(data_dim, variance=1.0):
   """Returns an independent normal with event size the size of data_dim.
 
   Args:
     data_dim: List of data dimensions.
+    variance: A scalar that is used as the diagonal entries of the covariance
+      matrix.
 
   Returns:
     Independent normal distribution.
@@ -262,5 +264,5 @@ def get_independent_normal(data_dim):
   return tfd.Independent(
       tfd.Normal(
           loc=tf.zeros(data_dim, dtype=tf.float32),
-          scale=tf.ones(data_dim, dtype=tf.float32)),
+          scale=tf.ones(data_dim, dtype=tf.float32)*tf.math.sqrt(variance)),
       reinterpreted_batch_ndims=len(data_dim))

eim/small_problems.py
@@ -26,6 +26,7 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 
 
+from eim.models import base
 from eim.models import his
 from eim.models import lars
 from eim.models import nis
@@ -42,6 +43,8 @@ flags.DEFINE_boolean("lars_allow_eval_target", False,
 flags.DEFINE_enum("target", dists.NINE_GAUSSIANS_DIST, dists.TARGET_DISTS,
                   "Distribution to draw data from.")
 flags.DEFINE_float(
+    "proposal_variance", 1.0, "Variance for the proposal")
+flags.DEFINE_float(
     "nine_gaussians_variance", 0.01,
     "Variance for the mixture components in the nine gaussians.")
 flags.DEFINE_string(
@@ -50,13 +53,13 @@ flags.DEFINE_string(
     "separated list.")
 flags.DEFINE_integer("his_t", 5,
                      "Number of steps for hamiltonian importance sampling.")
-flags.DEFINE_float("his_stepsize", 1e-2,
+flags.DEFINE_float("his_stepsize", 1e-1,
                    "Stepsize for hamiltonian importance sampling.")
 flags.DEFINE_float("his_alpha", 0.995,
                    "Alpha for hamiltonian importance sampling.")
-flags.DEFINE_boolean("his_learn_stepsize", False,
+flags.DEFINE_boolean("his_learn_stepsize", True,
                      "Allow HIS to learn the stepsize")
-flags.DEFINE_boolean("his_learn_alpha", False, "Allow HIS to learn alpha.")
+flags.DEFINE_boolean("his_learn_alpha", True, "Allow HIS to learn alpha.")
 flags.DEFINE_float("learning_rate", 3e-4,
                    "The learning rate to use for ADAM or SGD.")
 flags.DEFINE_integer("batch_size", 128, "The number of examples per batch.")
@@ -82,11 +85,12 @@ FLAGS = flags.FLAGS
 
 
 def exp_name():
-  return "target-%s.algo-%s.K-%d.run-%d" % (
+  return "target-%s.algo-%s.K-%d.run-%d-provar-%0.2f" % (
       FLAGS.target,
       FLAGS.algo,
       FLAGS.K,
       FLAGS.run,
+      FLAGS.proposal_variance
   )
 
 
@@ -101,11 +105,7 @@ def density_image_summary(log_density, num_points, title):
     num_points: Number of points in the grid.
     title: Title of the summary.
   """
-  if (FLAGS.target == dists.NINE_GAUSSIANS_DIST or
-      FLAGS.target == dists.TWO_RINGS_DIST):
-    bounds = (-2, 2)
-  elif FLAGS.target == dists.CHECKERBOARD_DIST:
-    bounds = (0, 1)
+  bounds = (-2, 2)
 
   x = tf.range(
       bounds[0], bounds[1], delta=(bounds[1] - bounds[0]) / float(num_points))
@@ -135,10 +135,7 @@ def density_image_summary(log_density, num_points, title):
 
 def sample_image_summary(model, title, num_samples=100000, num_bins=50):
   """Creates a summary plot approximating the density with samples."""
-  if FLAGS.target == dists.NINE_GAUSSIANS_DIST or FLAGS.target == dists.TWO_RINGS_DIST:
-    bounds = (-2, 2)
-  elif FLAGS.target == dists.CHECKERBOARD_DIST:
-    bounds = (0, 1)
+  bounds = (-2, 2)
   data = model.sample([num_samples])
 
   def _hist2d(x, y):
@@ -168,8 +165,10 @@ def make_lars_graph(target_dist,  # pylint: disable=invalid-name
                     mlp_layers,
                     dtype=tf.float32):
   """Construct the training graph for LARS."""
+  proposal = base.get_independent_normal([2], FLAGS.proposal_variance)
   model = lars.LARS(
-      K=K, T=K, data_dim=[2], accept_fn_layers=mlp_layers, dtype=dtype)
+      K=K, T=K, data_dim=[2], accept_fn_layers=mlp_layers,
+      proposal=proposal, dtype=dtype)
 
   train_data = target_dist.sample(batch_size)
   log_p = model.log_prob(train_data)
@@ -258,22 +257,21 @@ def main(unused_argv):
           mlp_layers=energy_fn_layers,
           dtype=tf.float32)
     else:
+      proposal = base.get_independent_normal([2], FLAGS.proposal_variance)
       if FLAGS.algo == "nis":
         print("Running NIS")
         model = nis.NIS(
-            K=FLAGS.K, data_dim=[2], energy_hidden_sizes=energy_fn_layers)
+            K=FLAGS.K, data_dim=[2], energy_hidden_sizes=energy_fn_layers,
+            proposal=proposal)
         density_image_summary(
             lambda x:  # pylint: disable=g-long-lambda
             (tf.squeeze(model.energy_fn(x)) + model.proposal.log_prob(x)),
             FLAGS.density_num_bins, "energy/nis")
       elif FLAGS.algo == "rejection_sampling":
         print("Running Rejection Sampling")
-        logit_accept_fn = tf.keras.Sequential([
-            tf.keras.layers.Dense(layer_size, activation="tanh")
-            for layer_size in energy_fn_layers
-        ] + [tf.keras.layers.Dense(1, activation=None)])
         model = rejection_sampling.RejectionSampling(
-            T=FLAGS.K, data_dim=[2], logit_accept_fn=logit_accept_fn)
+            T=FLAGS.K, data_dim=[2], energy_hidden_sizes=energy_fn_layers,
+            proposal=proposal)
         density_image_summary(
             lambda x: tf.squeeze(  # pylint: disable=g-long-lambda
                 tf.log_sigmoid(model.logit_accept_fn(x)), axis=-1) + model.
@@ -288,7 +286,8 @@ def main(unused_argv):
             init_step_size=FLAGS.his_stepsize,
             learn_stepsize=FLAGS.his_learn_stepsize,
             init_alpha=FLAGS.his_alpha,
-            learn_temps=FLAGS.his_learn_alpha)
+            learn_temps=FLAGS.his_learn_alpha,
+            proposal=proposal)
         density_image_summary(lambda x: -model.hamiltonian_potential(x),
                               FLAGS.density_num_bins, "energy/his")
         sample_image_summary(