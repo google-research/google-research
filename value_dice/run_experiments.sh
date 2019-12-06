66d30d7c1b20af6d34a2e789c2c9da92fef0f6e9
Ilya Kostrikov <kostrikov@google.com>
2019-12-05 17:38:27 -0800

    Code accompanying ValueDICE.
    PiperOrigin-RevId: 284091115

---
+++ value_dice/run_experiments.sh
@@ -0,0 +1,39 @@
+# Copyright 2019 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+declare -a env_names=("HalfCheetah-v2"  "Hopper-v2"  "Walker2d-v2" "Ant-v2")
+declare -a algos=("bc" "dac" "value_dice")
+
+expert_dir="./experts/"
+save_dir="./save/"
+
+for algo in "${algos[@]}"
+do
+  for env_name in "${env_names[@]}"
+  do
+    for ((seed=0;seed<10;seed+=1))
+    do
+      python -m value_dice.train_eval \
+        --expert_dir $expert_dir \
+        --save_dir $save_dir \
+        --algo $algo \
+        --env_name $env_name \
+        --seed $seed \
+        --num_trajectories 1 \
+        --alsologtostderr
+    done
+  done
+done
