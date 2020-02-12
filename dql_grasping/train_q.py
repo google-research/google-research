25d3092111b2f3fd3f9da84333f50cf78d11f367
Yanhua Sun <yanhuasun@google.com>
2020-02-11 16:27:33 -0800

    Internal change
    PiperOrigin-RevId: 294547609

--- dql_grasping/train_q.py
+++ dql_grasping/train_q.py
@@ -21,7 +21,7 @@ from __future__ import division
 from __future__ import print_function
 
 import gin.tf
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from tensorflow.contrib import framework as contrib_framework
 
 
