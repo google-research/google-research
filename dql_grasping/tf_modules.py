4876bb59ab76f66e4efb90515431c89df4db081e
Yanhua Sun <yanhuasun@google.com>
2020-02-14 13:01:17 -0800

    Explicitly replace "import tensorflow" with "tensorflow.compat.v1" for TF2.x migration
    PiperOrigin-RevId: 295207968

--- dql_grasping/tf_modules.py
+++ dql_grasping/tf_modules.py
@@ -21,7 +21,7 @@ from __future__ import division
 from __future__ import print_function
 
 import gin
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from tensorflow.contrib import layers as contrib_layers
 from tensorflow.contrib import slim as contrib_slim
 
