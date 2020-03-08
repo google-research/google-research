a050b0bbe42fd288a102e45d38b3ae39e1314a45
Tsung-Hsien Lee <tsunghsienlee@google.com>
2020-03-07 18:29:08 -0800

    Refactors run_sketches.cc
    PiperOrigin-RevId: 299615964

--- sketching/run_sketches.cc
+++ sketching/run_sketches.cc
@@ -16,16 +16,27 @@
 // Report error statistics, time taken and memory used.
 
 #include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
 #include <gflags/gflags.h>
 
+#include "absl/algorithm/container.h"
+#include "absl/memory/memory.h"
+#include "absl/random/bit_gen_ref.h"
+#include "absl/random/uniform_int_distribution.h"
+#include "absl/random/zipf_distribution.h"
 #include "countmin.h"
+#include "frequent.h"
 #include "lossy_count.h"
 #include "lossy_weight.h"
-#include "frequent.h"
-#include "absl/memory/memory.h"
-#include "absl/random/random.h"
+#include "sketch.h"
+#include "utils.h"
 
 DEFINE_int32(stream_size, 1000000, "Number of items in the stream");
 DEFINE_int32(lg_stream_range, 20, "Stream elements in 0..2^log_stream_range");
@@ -38,10 +49,10 @@ DEFINE_int32(frequent_size, 2000, "Items in memory for Frequent (Misra-Gries)");
 
 namespace sketch {
 
-void CreateStream(std::vector<IntFloatPair>* data, std::vector<float>* counts) {
-  data->reserve(FLAGS_stream_size);
+std::pair<std::vector<IntFloatPair>, std::vector<float>> CreateStream() {
+  std::vector<IntFloatPair> data(FLAGS_stream_size);
   uint stream_range = 1 << FLAGS_lg_stream_range;
-  counts->resize(stream_range);
+  std::vector<float> counts(stream_range, 0.0);
   BitGenerator bit_gen;
   absl::BitGenRef& gen = *bit_gen.BitGen();
   absl::zipf_distribution<uint> zipf(stream_range, FLAGS_zipf_param, 1.0);
@@ -49,19 +60,10 @@ void CreateStream(std::vector<IntFloatPair>* data, std::vector<float>* counts) {
   ULONG b = absl::uniform_int_distribution<int>(0, stream_range - 1)(gen);
   for (int i = 0; i < FLAGS_stream_size; ++i) {
     const auto& k = Hash(a, b, zipf(gen), stream_range);
-    counts->at(k) += 1.0;
-    data->push_back(std::make_pair(k, 1.0));
+    counts.at(k) += 1.0;
+    data.emplace_back(k, 1.0);
   }
-}
-
-int HeavyHittersExact(const std::vector<float>& counts, float thresh) {
-  int res = 0;
-  for (int i = 0; i < counts.size(); ++i) {
-    if (counts[i] > thresh) {
-      res++;
-    }
-  }
-  return res;
+  return {data, counts};
 }
 
 struct SketchStats {
@@ -79,9 +81,8 @@ struct SketchStats {
   ULONG estimate_time;
 };
 
-void TestSketch(Sketch* sketch,
-                const std::vector<IntFloatPair>& data,
-                const std::vector<float>& counts,
+void TestSketch(float threshold, const std::vector<IntFloatPair>& data,
+                const std::vector<float>& counts, Sketch* sketch,
                 SketchStats* stats) {
   auto start = std::chrono::high_resolution_clock::now();
   for (const auto& kv : data) {
@@ -91,8 +92,7 @@ void TestSketch(Sketch* sketch,
   stats->add_time = std::chrono::duration_cast<std::chrono::microseconds>(
       end_add - start).count();
   sketch->ReadyToEstimate();
-  sketch->HeavyHitters(FLAGS_stream_size * FLAGS_epsilon + 1e-6,
-                       &stats->heavy_hitters);
+  sketch->HeavyHitters(threshold, &stats->heavy_hitters);
   auto end_hh = std::chrono::high_resolution_clock::now();
   stats->hh_time = std::chrono::duration_cast<std::chrono::microseconds>(
       end_hh - end_add).count();
@@ -114,37 +114,12 @@ void TestSketch(Sketch* sketch,
                          stats->error_mean * stats->error_mean);
 }
 
-void Evaluate(const std::vector<float>& counts,
+void Evaluate(float threshold, const std::vector<float>& counts,
               int heavy_hitters, SketchStats* stats) {
-  float correct = 0;
-  float threshold = FLAGS_stream_size * FLAGS_epsilon + 1e-6;
-  for (uint k : stats->heavy_hitters) {
-    if (counts[k] > threshold) correct += 1;
-  }
-  stats->precision = correct / stats->heavy_hitters.size();
-  stats->recall = correct / heavy_hitters;
-}
-
-void PrintEval(const std::vector<float>& counts,
-               const std::vector<uint>& heavy_hitters,
-               Sketch* s) {
-  float threshold = FLAGS_stream_size * FLAGS_epsilon + 1e-6;
-  int j = 0;
-  for (int i = 0; i < counts.size(); ++i) {
-    if (j < heavy_hitters.size() && i == heavy_hitters[j]) {
-      if (counts[i] > threshold) {
-        printf("Found %d, Actual %.2f, Estimate %.2f\n", i, counts[i],
-               s->Estimate(i));
-      } else {
-        printf("FALSE POSITIVE %d, Actual %.2f, Estimate %.2f\n",
-               i, counts[i], s->Estimate(i));
-      }
-      j++;
-    } else if (counts[i] > threshold) {
-      printf("MISSED %d, Actual %.2f, Estimate %.2f\n", i, counts[i],
-             s->Estimate(i));
-    }
-  }
+  float correct = absl::c_count_if(
+      stats->heavy_hitters, [&](uint k) { return counts[k] > threshold; });
+  stats->precision = static_cast<float>(correct) / stats->heavy_hitters.size();
+  stats->recall = static_cast<float>(correct) / heavy_hitters;
 }
 
 void PrintOutput(const std::vector<SketchStats>& stats) {
@@ -159,52 +134,52 @@ void PrintOutput(const std::vector<SketchStats>& stats) {
 }
 
 void TestCounts() {
-  std::vector<IntFloatPair> data;
-  std::vector<float> counts;
-  CreateStream(&data, &counts);
-  int heavy_hitters = HeavyHittersExact(
-      counts, FLAGS_epsilon * FLAGS_stream_size + 1e-6);
+  auto [data, counts] = CreateStream();
+  const float threshold = FLAGS_epsilon * FLAGS_stream_size + 1e-6;
+  int heavy_hitters = absl::c_count_if(counts, [&](float c) {
+    return c > threshold;
+  });
   printf("\nStream size: %d, Stream range: 2^%d\n", FLAGS_stream_size,
          FLAGS_lg_stream_range);
   printf("There were %d elements above threshold %0.2f, for e = %f\n\n",
          heavy_hitters, FLAGS_epsilon * FLAGS_stream_size, FLAGS_epsilon);
 
-  std::vector<std::pair<std::string, std::unique_ptr<Sketch> > > sketches;
+  std::vector<std::pair<std::string, std::unique_ptr<Sketch>>> sketches;
 
-  sketches.push_back(std::make_pair(
+  sketches.emplace_back(
       "CM", absl::make_unique<CountMin>(
-          CountMin(FLAGS_hash_count, FLAGS_hash_size))));
-  sketches.push_back(std::make_pair(
+          CountMin(FLAGS_hash_count, FLAGS_hash_size)));
+  sketches.emplace_back(
       "CM_CU", absl::make_unique<CountMinCU>(
-          CountMinCU(FLAGS_hash_count, FLAGS_hash_size))));
-  sketches.push_back(std::make_pair(
+          CountMinCU(FLAGS_hash_count, FLAGS_hash_size)));
+  sketches.emplace_back(
       "LC", absl::make_unique<LossyCount>(LossyCount(
-          (int)(1.0 / FLAGS_epsilon)))));
-  sketches.push_back(std::make_pair(
+          (int)(1.0 / FLAGS_epsilon))));
+  sketches.emplace_back(
       "LC_FB", absl::make_unique<LossyCount_Fallback>(LossyCount_Fallback(
-          (int)(1.0 / FLAGS_epsilon), FLAGS_hash_count, FLAGS_hash_size))));
-  sketches.push_back(std::make_pair(
+          (int)(1.0 / FLAGS_epsilon), FLAGS_hash_count, FLAGS_hash_size)));
+  sketches.emplace_back(
       "LW", absl::make_unique<LossyWeight>(LossyWeight(
-          FLAGS_frequent_size, FLAGS_hash_count, FLAGS_hash_size))));
-  sketches.push_back(std::make_pair(
-      "Freq", absl::make_unique<Frequent>(Frequent(FLAGS_frequent_size))));
-  sketches.push_back(std::make_pair(
+          FLAGS_frequent_size, FLAGS_hash_count, FLAGS_hash_size)));
+  sketches.emplace_back(
+      "Freq", absl::make_unique<Frequent>(Frequent(FLAGS_frequent_size)));
+  sketches.emplace_back(
       "Freq_FB", absl::make_unique<Frequent_Fallback>(Frequent_Fallback(
-      FLAGS_frequent_size, FLAGS_hash_count, FLAGS_hash_size))));
-  sketches.push_back(std::make_pair(
+      FLAGS_frequent_size, FLAGS_hash_count, FLAGS_hash_size)));
+  sketches.emplace_back(
       "CMH", absl::make_unique<CountMinHierarchical>(CountMinHierarchical(
-          FLAGS_hash_count, FLAGS_hash_size, FLAGS_lg_stream_range))));
-  sketches.push_back(std::make_pair(
+          FLAGS_hash_count, FLAGS_hash_size, FLAGS_lg_stream_range)));
+  sketches.emplace_back(
       "CMH_CU", absl::make_unique<CountMinHierarchicalCU>(
           CountMinHierarchicalCU(
-              FLAGS_hash_count, FLAGS_hash_size, FLAGS_lg_stream_range))));
+              FLAGS_hash_count, FLAGS_hash_size, FLAGS_lg_stream_range)));
 
   std::vector<SketchStats> sketch_stats;
-  for (auto& sketch : sketches) {
+  for (const auto& [name, sketch] : sketches) {
     SketchStats s;
-    s.name = sketch.first;
-    TestSketch(sketch.second.get(), data, counts, &s);
-    Evaluate(counts, heavy_hitters, &s);
+    s.name = name;
+    TestSketch(threshold, data, counts, sketch.get(), &s);
+    Evaluate(threshold, counts, heavy_hitters, &s);
     sketch_stats.push_back(s);
   }
 
