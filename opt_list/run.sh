4d15ed8ff4c72a40920b8637b5153078139f718f
Luke Metz <lmetz@google.com>
2020-03-10 09:58:13 -0700

    Fixed tests breaking + added tests for the rest of the examples.
    PiperOrigin-RevId: 300115128

--- opt_list/run.sh
+++ opt_list/run.sh
@@ -18,7 +18,10 @@ set -x
 virtualenv -p python3 .
 source ./bin/activate
 
-pip3 install tensorflow torch
+pip3 install tensorflow torch jax jaxlib
+pip3 install git+https://github.com/google-research/flax.git@prerelease
 
 python3 -m opt_list.examples.tf_v1
 python3 -m opt_list.examples.tf_keras
+python3 -m opt_list.examples.torch
+python3 -m opt_list.examples.jax
