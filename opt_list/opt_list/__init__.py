1f7bb5f22562cd17e52ce5660f2d4cc7a60894b8
Luke Metz <lmetz@google.com>
2020-03-12 13:50:27 -0700

    Make pip installable after a git clone.
    PiperOrigin-RevId: 300616930

--- opt_list/opt_list/__init__.py
+++ opt_list/opt_list/__init__.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # Copyright 2020 The Google Research Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,20 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#! /bin/bash
-set -e
-set -x
-virtualenv -p python3 .
-source ./bin/activate
-
-pip3 install tensorflow torch jax jaxlib
-pip3 install git+https://github.com/google-research/flax.git@prerelease
-
-cd opt_list/
-
-python3 -m opt_list.examples.tf_v1
-python3 -m opt_list.examples.tf_keras
-python3 -m opt_list.examples.torch
-python3 -m opt_list.examples.jax_flax
-python3 -m opt_list.examples.jax_optimizers
-python3 -m opt_list.examples.jax_optix
