89667ae240e368b6dba38e74a6d848a86d1dcbd4
Alessandro Epasto <aepasto@google.com>
2020-05-27 09:45:54 -0700

    Implementation of the code needed to replicate the experiments of the AISTATS2020 Paper titled "Fair Correlation Clustering".
    PiperOrigin-RevId: 313406841

---
+++ correlation_clustering/correlation_clustering_solver.py
@@ -0,0 +1,137 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Implementations of standard correlation clustering algorithm.
+"""
+
+import collections
+import random
+from .utils import CorrelationClusteringError
+
+
+def PivotAlgorithm(graph):
+  """The well-known pivot algorithm for correlation clustering.
+
+  Run the pivot algorithm on graph.
+  Args:
+    graph: the graph in nx.Graph format.
+  Returns:
+    The solution.
+  """
+  # This is to ensure consistency of random runs with same seed.
+  nodes = sorted(list(graph.nodes()))
+  random.shuffle(nodes)
+  clusters = []
+  clustered = set()
+
+  for node in nodes:
+    if node in clustered:
+      continue
+    cluster = [node]
+    clustered.add(node)
+
+    for neighbor in graph.neighbors(node):
+      if graph.edges[node,
+                     neighbor]['weight'] > 0 and neighbor not in clustered:
+        cluster.append(neighbor)
+        clustered.add(neighbor)
+    clusters.append(cluster)
+  assert len(clustered) == sum(len(c) for c in clusters)
+  assert clustered == set(nodes)
+  return clusters
+
+
+def LocalSearchAlgorithm(graph, attempts=10):
+  """Run the local search heuristic for correlation clustering.
+
+  The algorithm is a simple local search heuristic that tries to improve the
+  clustering by local moves of individual nodes until a certain number of
+  iterations over the graph are completed.
+
+  Args:
+    graph: the graph in nx.Graph format.
+    attempts: number of times local search is run.
+  Returns:
+    The solution.
+  """
+  best_sol = None
+  best_sol_value = None
+  for _ in range(attempts):
+    ls = LocalSearchCorrelationClustering(graph, 20)
+    sol = ls.RunClustering()
+    cost = CorrelationClusteringError(graph, sol)
+    if best_sol_value is None or best_sol_value > cost:
+      best_sol_value = cost
+      best_sol = sol
+  return best_sol
+
+
+class LocalSearchCorrelationClustering(object):
+  """Single run of the the local search heuristic for correlation clustering.
+
+  The algorithm performs a series of passes over the nodes in the graph in
+  arbitrary order.
+  For each node in the order, it checks if the solution can be improved by
+  moving the node to another cluster.
+  """
+
+  def __init__(self, graph, iterations):
+    self.graph = graph
+    self.iterations = iterations
+    self.node_to_cluster_id = {}
+    self.cluster_uid = 0
+    self.cluster_id_nodes = collections.defaultdict(set)
+    for node in self.graph.nodes():
+      self.node_to_cluster_id[node] = self.cluster_uid
+      self.cluster_id_nodes[self.cluster_uid].add(node)
+      self.cluster_uid += 1
+
+  def MoveNodeToCluster(self, node, cluster_id):
+    """Moves a node to a cluster."""
+    self.cluster_id_nodes[self.node_to_cluster_id[node]].remove(node)
+    self.node_to_cluster_id[node] = cluster_id
+    self.cluster_id_nodes[cluster_id].add(node)
+
+  def DoOnePassMoves(self):
+    """Completes one pass over the graph."""
+    nodes = sorted(list(self.graph.nodes()))
+    random.shuffle(nodes)
+    for node in nodes:
+      positive_to_clusters = collections.defaultdict(int)
+      best_cluster = None
+      best_cluster_cost = self.graph.number_of_nodes() + 1
+      positives = 0
+      for neighbor in self.graph.neighbors(node):
+        if self.graph.edges[node, neighbor]['weight'] > 0:
+          positives += 1
+          positive_to_clusters[self.node_to_cluster_id[neighbor]] += 1
+      curr_cluster = self.node_to_cluster_id[node]
+      curr_cluster_cost = positives + len(
+          self.cluster_id_nodes[curr_cluster]
+          )-1-2*positive_to_clusters[curr_cluster]
+      for c, pos in positive_to_clusters.items():
+        if c != curr_cluster:
+          cluster_cost = positives + len(self.cluster_id_nodes[c]) - 2*pos
+          if cluster_cost < best_cluster_cost:
+            best_cluster_cost = cluster_cost
+            best_cluster = c
+
+      if best_cluster_cost < curr_cluster_cost:
+        self.MoveNodeToCluster(node, best_cluster)
+
+  def RunClustering(self):
+    for _ in range(self.iterations):
+      self.DoOnePassMoves()
+    return self.cluster_id_nodes.values()
---
+++ correlation_clustering/utils.py
@@ -0,0 +1,131 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utils for the fair correlation clustering algorithm.
+"""
+
+import collections
+import math
+import numpy as np
+import sklearn.metrics
+
+
+def BooleanVectorsFromGraph(graph):
+  """Create a boolean encoding for the nodes in the graph.
+
+  Starting from the graph, it creates a set of boolean vectors where u,v,
+  has an entry 1 for each positive edge (0 for negative edge). Selfloops
+  are assumed positive.
+
+  Args:
+    graph: graph in nx.Graph format.
+  Returns:
+    the nxn bolean matrix with the encoding.
+  """
+  n = graph.number_of_nodes()
+  vectors = np.identity(n)
+  for u, v, d in graph.edges(data=True):
+    if d['weight'] > 0:
+      vectors[u][v] = 1
+      vectors[v][u] = 1
+  return vectors
+
+
+def PairwiseFairletCosts(graph):
+  """Create a matrix with the fairlet cost.
+
+  Args:
+    graph: graph in nx.Graph format.
+  Returns:
+    the nxn matrix with the fairlet cost for each pair of nodes.
+  """
+  assert max(list(graph.nodes())) == graph.number_of_nodes() - 1
+  assert min(list(graph.nodes())) == 0
+
+  bool_vectors = BooleanVectorsFromGraph(graph)
+  distance_matrix = sklearn.metrics.pairwise_distances(
+      bool_vectors, metric='l1')
+  # This counts twice the negative edge inside each u,v fairlet, so we deduct
+  # one for each such pair.
+  for u, v, d in graph.edges(data=True):
+    if d['weight'] < 0:
+      distance_matrix[u][v] -= 1
+      distance_matrix[v][u] -= 1
+  return distance_matrix
+
+
+def ClusterIdMap(solution):
+  """Create a map from node to cluster id.
+
+  Args:
+    solution: list of clusters.
+  Returns:
+    the map from node id to cluster id.
+  """
+  clust_assignment = {}
+  for i, clust in enumerate(solution):
+    for elem in clust:
+      clust_assignment[elem] = i
+  return clust_assignment
+
+
+def FractionalColorImbalance(graph, solution, alpha):
+  """Evaluates the color imbalance of solution.
+
+  Computes the fraction of nodes that are above the threshold for color
+  representation.
+
+  Args:
+    graph: in nx.Graph format.
+    solution: list of clusters.
+    alpha: representation constraint.
+  Returns:
+    the fraction of nodes that are above the threshold for color.
+  """
+  total_violation = 0
+  nodes = 0
+  for cluster in solution:
+    color_count = collections.defaultdict(int)
+    for elem in cluster:
+      color_count[graph.nodes[elem]['color']] += 1
+    for count in color_count.values():
+      imbalance = max(0, count - math.floor(float(len(cluster)) * alpha))
+      total_violation += imbalance
+    nodes += len(cluster)
+  return 1.0 * total_violation / nodes
+
+
+def CorrelationClusteringError(graph, solution):
+  """Evaluates  the correlation clustering error of solution.
+
+  Computes the fraction of edges that are misclassified by the algorithm.
+
+  Args:
+    graph: in nx.Graph format.
+    solution: list of clusters.
+  Returns:
+    the fraction of edges that are incorrectly classified.
+  """
+  clust_assignment = ClusterIdMap(solution)
+  errors = 0
+  corrects = 0
+  for u, v, d in graph.edges(data=True):
+    if (d['weight'] > 0 and clust_assignment[u] != clust_assignment[v]) or \
+        (d['weight'] < 0 and clust_assignment[u] == clust_assignment[v]):
+      errors += 1
+    elif (d['weight'] > 0 and clust_assignment[u] == clust_assignment[v]) or \
+        (d['weight'] < 0 and clust_assignment[u] != clust_assignment[v]):
+      corrects += 1
+  return float(errors) / (errors + corrects)
