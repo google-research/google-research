deeacdcc1bbf433d2e202a01fb7ef3ae6c314b99
Rishabh Agarwal <rishabhagarwal@google.com>
2020-06-03 08:41:53 -0700

    Port code to python3.
    PiperOrigin-RevId: 314537527

--- meta_reward_learning/textworld/lib/helpers.py
+++ meta_reward_learning/textworld/lib/helpers.py
@@ -13,11 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Lint as: Python3
 """Helper functions for environment and agent class."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import collections
 import itertools
