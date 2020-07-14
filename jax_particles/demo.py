eca008905febafc5a803136f17cd1126590710d0
Mark Woodward <markwoodward@google.com>
2020-07-14 15:48:05 -0700

    A 2D physics simulator for collidable circular objects
    PiperOrigin-RevId: 321251052

---
+++ jax_particles/demo.py
@@ -0,0 +1,131 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Demonstrates usage of jax_particles."""
+import time
+import jax
+import jax.numpy as jnp
+from jax_particles import BaseEnvironment
+from jax_particles import Entity
+from jax_particles.renderer import ThreadedRenderer
+
+
+class Environment(BaseEnvironment):
+  """A simple demo environment. Typically you would create a module for this."""
+
+  def __init__(self):
+    super().__init__()
+
+    # add agents
+    agents = []
+    agent = Entity()
+    agent.name = "agent"
+    agent.color = [64, 64, 64]
+    agent.collideable = True
+    agents.append(agent)
+
+    # add collideable objects
+    objects = []
+    obj = Entity()
+    obj.name = "object"
+    obj.color = [64, 64, 204]
+    obj.collideable = True
+    objects.append(obj)
+
+    # add landmarks
+    landmarks = []
+    landmark = Entity()
+    landmark.name = "landmark"
+    landmark.color = [64, 205, 64]
+    landmark.collideable = False
+    landmark.radius = 0.1
+    landmarks.append(landmark)
+
+    self.entities.extend(agents)
+    self.entities.extend(objects)
+    self.entities.extend(landmarks)
+
+    self.a_shape = (1, 2)  # shape of actions, one agent here
+    self.o_shape = {"vec": None}  # shape of observations, no observations here
+
+    self._compile()
+
+  def init_state(self, rng):
+    """Returns a state (p,v,misc) tuple."""
+    shape = (len(self.entities), 2)
+    p = jax.random.uniform(rng, shape) * (self.max_p - self.min_p) + self.min_p
+    v = jnp.zeros(shape)+0.0
+    return (p, v, None)
+
+  def obs(self, s):
+    """Returns an observation dictionary."""
+    o = {"vec": None}
+    return o
+
+  def reward(self, s):
+    """Returns a joint reward: np.float32."""
+    return 0.0
+
+
+def main():
+  """Create an environment and step through, taking user actions."""
+  batch_size = 5  # 100000
+  i_batch = 1  # 25843
+  stop_every_step = False
+
+  env = Environment()
+  renderer = ThreadedRenderer()
+
+  # compile a step function
+  def step(s, a):
+    s = env.step(s, a)
+    o = env.obs(s)
+    r = env.reward(s)
+    return (s, o, r)
+  step = jax.vmap(step)
+  step = jax.jit(step)
+
+  # compile an init_state function
+  init_state = env.init_state
+  init_state = jax.vmap(init_state)
+  init_state = jax.jit(init_state)
+
+  rng = jax.random.PRNGKey(1)
+  s = init_state(jax.random.split(rng, batch_size))
+  last_time = time.time()
+  while True:
+    # extract the state of the i'th environment
+    s_i = [elem[i_batch, :] if elem is not None else None for elem in s]
+
+    # render
+    renderer.render(env, s_i)
+
+    # get user action
+    a = renderer.get_action()
+    a = jnp.broadcast_to(jnp.array(a), (batch_size,)+env.a_shape)
+
+    # do simulation step
+    s, o, r = step(s, a)  # pylint: disable=unused-variable
+
+    if stop_every_step:
+      input("> ")
+    else:
+      while time.time() - last_time < env.dt:
+        time.sleep(0.001)
+      last_time = time.time()
+
+
+if __name__ == "__main__":
+  main()
---
+++ jax_particles/jax_particles/renderer.py
@@ -0,0 +1,154 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Simple interface for displaying state and accepting commands from user."""
+import threading
+import cv2
+import inputs
+import numpy as np
+
+
+class BaseRenderer(object):
+  """The base class for rendering a scene."""
+
+  def render(self, env, env_state, trajs=None, hide_state=False):
+    raise NotImplementedError()
+
+
+class ThreadedRenderer(BaseRenderer):
+  """A threaded renderer, that receives commands from user."""
+
+  def __init__(self, width=200):
+    self._width = width
+    self._img = None
+    self._action = np.array([0.0, 0.0])
+    self._display_thread = threading.Thread(target=self.display_fn, daemon=True)
+    self._display_thread.start()
+    if not inputs.devices.gamepads:
+      self._joystick_thread = threading.Thread(target=self.joystick_fn,
+                                               daemon=True)
+      self._joystick_thread.start()
+
+  def display_fn(self):
+    curr_img = None
+    cv2.namedWindow("jax_particles", cv2.WINDOW_GUI_NORMAL|cv2.WINDOW_AUTOSIZE)
+    while True:
+      if curr_img is not self._img:
+        curr_img = self._img
+        cv2.imshow("jax_particles", curr_img)
+      key = cv2.waitKey(10)
+      if key != -1:
+        self._action = key_to_action(key)
+
+  def joystick_fn(self):
+    while True:
+      events = inputs.get_gamepad()
+      for event in events:
+        if event.ev_type == "Absolute" and event.code == "ABS_Z":  # X value
+          self._action[0] = (event.state-127.5)/127.5
+        if event.ev_type == "Absolute" and event.code == "ABS_RZ":  # Y value
+          self._action[1] = (event.state-127.5)/127.5
+
+  def render(self, env, env_state, trajs=None, hide_state=False):
+    # trajs are a list of lists of s,o,a,r dictionaries
+    self._img = draw_scene(env, env_state, trajs, self._width, hide_state)
+
+  def get_action(self):
+    action = self._action
+    return action
+
+
+def draw_scene(env, env_state, trajs, width, hide_state):
+  """Draws the scene of a state with trajectories overlayed."""
+  if trajs is None:
+    trajs = []
+  scale = width/(env.max_p[0] - env.min_p[0])
+  height = (int)((env.max_p[1] - env.min_p[1])*scale)
+  offset = np.array([width/2.0, height/2.0])
+
+  img = (255*np.ones((width, height, 3))).astype(np.uint8)
+
+  for traj in trajs:
+    img = draw_traj(img, env, traj, width)
+
+  if not hide_state:
+    p = env_state[0]
+    for i, entity in enumerate(env.entities):
+      pos = tuple((p[i, :]*scale + offset).astype(dtype=np.int32))
+      rad = (int)(entity.radius*scale)
+      color = entity.color
+      color_alpha = entity.color_alpha
+      img = draw_circle(img, pos, rad, color, color_alpha)
+  return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+def draw_circle(img, pos, rad, color, alpha):
+  """Generic circle drawing function."""
+  overlay = img.copy()
+  cv2.circle(overlay, pos, rad, color, -1, lineType=cv2.cv2.LINE_AA)
+  return cv2.addWeighted(overlay, alpha, img, 1-alpha, 0)
+
+
+def draw_line(img, start, end, color, alpha):
+  """Generic line drawing function."""
+  overlay = img.copy()
+  cv2.line(overlay, start, end, color, 1, lineType=cv2.cv2.LINE_AA)
+  return cv2.addWeighted(overlay, alpha, img, 1-alpha, 0)
+
+
+def draw_traj(img, env, traj, width, color=None):
+  """Draw an individual trajectory."""
+  if len(traj) > 1:
+    scale = width/(env.max_x - env.min_x)
+    height = (int)((env.max_y - env.min_y)*scale)
+    offset = np.array([width/2.0, height/2.0])
+    initial_state = env.get_state()  # to reset when done
+
+    entities = env.get_entities()
+
+    env.set_state(traj[0]["s"])
+    old_entity_states = [entity.state for entity in env.get_entities()]
+    for i in range(1, len(traj)):
+      env.set_state(traj[i]["s"])
+      entity_states = [entity.state for entity in env.get_entities()]
+
+      for elems in zip(entities, old_entity_states, entity_states):
+        entity, entity_state_start, entity_state_end = elems
+        pos_start = tuple((entity_state_start.p*scale + offset).astype(np.int))
+        pos_end = tuple((entity_state_end.p*scale + offset).astype(np.int))
+        if pos_start[0] != pos_end[0] or pos_start[1] != pos_end[1]:
+          color = (tuple((entity.color*255).astype(np.int).tolist())
+                   if color is None else color)
+          img = draw_line(img, pos_start, pos_end, color, entity.color_alpha)
+
+      old_entity_states = entity_states
+
+    env.set_state(initial_state)
+  return img
+
+
+def key_to_action(key):
+  """Maps keyboard keys to actions."""
+  action = np.array([0.0, 0.0])
+  if key == ord("a") or key == 81:  # left
+    action[0] -= 1.0
+  if key == ord("w") or key == 82:  # up
+    action[1] -= 1.0
+  if key == ord("d") or key == 83:  # right
+    action[0] += 1.0
+  if key == ord("s") or key == 84:  # down
+    action[1] += 1.0
+
+  return action
