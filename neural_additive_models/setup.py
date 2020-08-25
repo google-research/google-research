2abd18a85880bb3afde313f80d764db0032b398f
Rishabh Agarwal <rishabhagarwal@google.com>
2020-08-24 21:09:08 -0400

    Fix sklearn version

--- neural_additive_models/setup.py
+++ neural_additive_models/setup.py
@@ -25,7 +25,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 install_requires = [
     'tensorflow>=1.15',
     'numpy>=1.15.2',
-    'sklearn>=0.23',
+    'sklearn>=0.19.2',
     'pandas>=0.24',
     'absl-py',
 ]
