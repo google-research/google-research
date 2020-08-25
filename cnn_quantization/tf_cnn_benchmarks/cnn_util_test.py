e0cd21f547fc21afce7eb382db0f8b689e27b9a1
Lisa Wang <wanglisa@google.com>
2020-08-25 13:38:49 -0700

    Upgraded targets from py2 to py3.
    * //third_party/google_research/google_research/cnn_quantization/tf_cnn_benchmarks:allreduce_test
    
    * //third_party/google_research/google_research/cnn_quantization/tf_cnn_benchmarks:cnn_util_test
    
    PiperOrigin-RevId: 328394674

--- cnn_quantization/tf_cnn_benchmarks/cnn_util_test.py
+++ cnn_quantization/tf_cnn_benchmarks/cnn_util_test.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Lint as: python2, python3
 """Tests for tf_cnn_benchmarks.cnn_util."""
 
 from __future__ import absolute_import
@@ -22,6 +23,7 @@ from __future__ import print_function
 import threading
 import time
 
+from six.moves import range
 import tensorflow.compat.v1 as tf
 
 from cnn_quantization.tf_cnn_benchmarks import cnn_util
