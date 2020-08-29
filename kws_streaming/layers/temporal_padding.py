da6067c8b91e3d166abed1848d4af040ffd77a6f
Oleg Rybakov <rybakov@google.com>
2020-08-28 18:00:47 -0700

    temporal padding for tensor with rank >= 2
    PiperOrigin-RevId: 329041792

---
+++ kws_streaming/layers/temporal_padding.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""TemporalPadding layer."""
+from kws_streaming.layers.compat import tf
+from kws_streaming.layers.modes import Modes
+
+SUPPORTED_PADDINGS = ['valid', 'causal', 'same']
+
+
+class TemporalPadding(tf.keras.layers.Layer):
+  """Padding in time dimension of tensor with rank >= 2.
+
+  It is convenient for models with streaming support: in streaming mode
+  it will disable padding; and in non streaming mode it will pad input data
+  in time dimension [batch, time, ...].
+
+  Attributes:
+    mode: Training or inference modes: non streaming, streaming.
+    padding: padding mode supports 'causal' or 'same'. 'valid' - not padding
+    padding_size: how much to pad
+  """
+
+  def __init__(self,
+               mode=Modes.TRAINING,
+               padding_size=None,
+               padding=None,
+               **kwargs):
+    super(TemporalPadding, self).__init__(**kwargs)
+
+    if padding not in SUPPORTED_PADDINGS:
+      raise ValueError('wrong padding ', padding)
+
+    if mode not in [Modes.TRAINING, Modes.NON_STREAM_INFERENCE,
+                    Modes.STREAM_INTERNAL_STATE_INFERENCE,
+                    Modes.STREAM_EXTERNAL_STATE_INFERENCE]:
+      raise ValueError('wrong mode ', mode)
+
+    self.mode = mode
+    self.padding = padding
+    self.padding_size = padding_size
+
+  def call(self, inputs):
+
+    if inputs.shape.rank < 2:
+      raise ValueError('inputs.shape.rank: %d must be >= 2' % inputs.shape.rank)
+
+    if self.mode in [
+        Modes.STREAM_INTERNAL_STATE_INFERENCE,
+        Modes.STREAM_EXTERNAL_STATE_INFERENCE
+    ] or self.padding == 'valid':
+      # padding is not applied in streaming mode or on valid
+      return inputs
+
+    pad = [[0, 0]] * inputs.shape.rank
+
+    if self.padding == 'causal':
+      pad[1] = [self.padding_size, 0]
+    elif self.padding == 'same':
+      half = self.padding_size // 2
+      pad[1] = [half, half]
+
+    inputs = tf.pad(inputs, pad, 'constant')
+    return inputs
+
+  def get_config(self):
+    config = super(TemporalPadding, self).get_config()
+    config.update({
+        'mode': self.mode,
+        'padding': self.padding,
+        'padding_size': self.padding_size,
+    })
+    return config
+
+  def get_input_state(self):
+    return []
+
+  def get_output_state(self):
+    return []
---
+++ kws_streaming/layers/temporal_padding_test.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Tests for kws_streaming.layers.temporal_padding."""
+from absl.testing import parameterized
+import numpy as np
+from kws_streaming.layers import temporal_padding
+from kws_streaming.layers.compat import tf
+from kws_streaming.layers.compat import tf1
+from kws_streaming.layers.modes import Modes
+tf1.disable_eager_execution()
+
+
+class TemporalPaddingTest(tf.test.TestCase, parameterized.TestCase):
+
+  @parameterized.parameters(5, 0)
+  def test_padding(self, padding_size):
+    batch_size = 1
+    time_dim = 3
+    feature_dim = 3
+    padding = 'causal'
+    inputs = tf.keras.layers.Input(
+        shape=(time_dim, feature_dim), batch_size=batch_size)
+    net = temporal_padding.TemporalPadding(
+        padding=padding, padding_size=padding_size)(
+            inputs)
+    model = tf.keras.Model(inputs, net)
+
+    np.random.seed(1)
+    input_signal = np.random.rand(batch_size, time_dim, feature_dim)
+    output_signal = model.predict(input_signal)
+    output_reference = tf.keras.backend.temporal_padding(
+        input_signal, padding=(padding_size, 0))
+    self.assertAllClose(output_signal, output_reference)
+    self.assertAllEqual(output_signal.shape,
+                        [batch_size, time_dim + padding_size, feature_dim])
+
+  def test_no_padding_in_streaming(self):
+    batch_size = 1
+    feature_dim = 3
+    padding = 'same'
+    padding_size = 5
+    mode = Modes.STREAM_EXTERNAL_STATE_INFERENCE
+    inputs = tf.keras.layers.Input(
+        shape=(1, feature_dim), batch_size=batch_size)
+    net = temporal_padding.TemporalPadding(
+        padding=padding, padding_size=padding_size, mode=mode)(
+            inputs)
+    self.assertAllEqual(
+        tf.keras.backend.int_shape(net), [batch_size, 1, feature_dim])
+
+
+if __name__ == '__main__':
+  tf.test.main()
