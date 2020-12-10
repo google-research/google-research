c7abe365563dcc63372d03161c2cca9d28e38e01
Aurko Roy <aurkor@google.com>
2020-12-09 22:16:13 -0800

    optimize relative attention by replacing matmuls with einsums
    PiperOrigin-RevId: 346710004

--- routing_transformer/utils.py
+++ routing_transformer/utils.py
@@ -2381,22 +2381,13 @@ def _relative_attention_inner(x, y, z, transpose):
   Returns:
     A Tensor with shape [batch_size, heads, length, length or depth].
   """
-  batch_size = tf.shape(x)[0]
-  heads = x.get_shape().as_list()[1]
-  length = tf.shape(x)[2]
-
   # xy_matmul is [batch_size, heads, length or 1, length or depth]
-  xy_matmul = tf.matmul(x, y, transpose_b=transpose)
-  # x_t is [length or 1, batch_size, heads, length or depth]
-  x_t = tf.transpose(x, [2, 0, 1, 3])
-  # x_t_r is [length or 1, batch_size * heads, length or depth]
-  x_t_r = tf.reshape(x_t, [length, heads * batch_size, -1])
-  # x_tz_matmul is [length or 1, batch_size * heads, length or depth]
-  x_tz_matmul = tf.matmul(x_t_r, z, transpose_b=transpose)
-  # x_tz_matmul_r is [length or 1, batch_size, heads, length or depth]
-  x_tz_matmul_r = tf.reshape(x_tz_matmul, [length, batch_size, heads, -1])
-  # x_tz_matmul_r_t is [batch_size, heads, length or 1, length or depth]
-  x_tz_matmul_r_t = tf.transpose(x_tz_matmul_r, [1, 2, 0, 3])
+  if transpose:
+    xy_matmul = tf.einsum("bhxd,bhyd->bhxy", x, y)
+    x_tz_matmul_r_t = tf.einsum("bhxd,xyd->bhxy", x, z)
+  else:
+    xy_matmul = tf.einsum("bhxd,bhdy->bhxy", x, y)
+    x_tz_matmul_r_t = tf.einsum("bhxd,xdy->bhxy", x, z)
   return xy_matmul + x_tz_matmul_r_t
 
 
