e180bda2ab93f0dfa77bb31e9e47d9b1cbb44944
Petros Maniatis <maniatis@google.com>
2021-01-20 18:28:53 -0800

    Changes CuBertDocument construction to allow empty sentences.
    Previously, CuBert sentence construction threw away empty lines from the middle of an input. This CL changes that behavior to emit empty lines as empty sentences.
    
    In Python, that's not quite true, because Python parses multiple newline characters in a row as one NEWLINE followed by multiple NL tokens. For Python's tokenization, this means that multiple empty lines will appear as NL tokens.
    
    Added tests show both the Python and the non-Python case.
    
    PiperOrigin-RevId: 352919413

--- cubert/code_to_subtokenized_sentences.py
+++ cubert/code_to_subtokenized_sentences.py
@@ -14,36 +14,16 @@
 # limitations under the License.
 
 """This modules demonstrates how to convert code to subtokenized sentences."""
-
 import itertools
-import json
 from typing import List, Text
 
-from absl import app
-from absl import flags
+
 from absl import logging
 from tensor2tensor.data_generators import text_encoder
 
+
 from cubert import cubert_tokenizer
-from cubert import tokenizer_registry
 from cubert import unified_tokenizer
-
-FLAGS = flags.FLAGS
-
-flags.DEFINE_string('vocabulary_filepath', None,
-                    'Path to the subword vocabulary.')
-
-flags.DEFINE_string('input_filepath', None,
-                    'Path to the Python source code file.')
-
-flags.DEFINE_string('output_filepath', None,
-                    'Path to the output file of subtokenized source code.')
-
-flags.DEFINE_enum_class(
-    'tokenizer',
-    default=tokenizer_registry.TokenizerEnum.PYTHON,
-    enum_class=tokenizer_registry.TokenizerEnum,
-    help='The tokenizer to use.')
 
 
 def code_to_cubert_sentences(
@@ -73,11 +53,38 @@ def code_to_cubert_sentences(
   # False) and NEWLINE tokens (key is True).
   groups_by_endtoken = itertools.groupby(
       tokens, key=lambda x: x == unified_tokenizer.NEWLINE)
-  # This will keep only the sublists that aren't just [NEWLINE]*, i.e., those
-  # that have key False. We call these raw_sentences, because they're not
-  # terminated.
-  raw_sentences = [list(v) for k, v in groups_by_endtoken if not k
-                  ]  # type: List[List[Text]]
+  # This will produce raw sentences from the groups. For groups that were made
+  # of non-NEWLINE tokens, the entire group makes up a single raw sentence.
+  # For groups of NEWLINE tokens (e.g., generated from consecutive empty lines),
+  # we ignore the first NEWLINE (since it terminated a preceding sentence) and
+  # create empty sentences for each of the rest.
+  #
+  # For example, [a, b, NEWLINE, c, d, NEWLINE] will yield the raw sentences
+  # [a, b], and [c, d].
+  # Instead, [a, b, NEWLINE, NEWLINE, NEWLINE, c, d, NEWLINE] will yield the
+  # raw sentences [a, b], [], [], [c, d].
+  #
+  # We call these raw_sentences, because they're not terminated.
+  raw_sentences: List[List[str]] = []
+  for k, v in groups_by_endtoken:
+    tokens = list(v)
+    if k:
+      # True means this was a NEWLINE group.
+      if len(tokens) == 1:
+        # This is just a terminator of the previous sentence. Just throw it out.
+        continue
+      elif len(tokens) > 1:
+        # This is a chain of NEWLINE tokens. The first we throw out, but we
+        # add empty sentences for the rest.
+        raw_sentences.extend([[]] * (len(tokens) - 1))
+      else:
+        # We shouldn't have a group of empty tokens. This should not happen.
+        raise AssertionError('itertools.groupby seems to have returned an '
+                             'empty group: %r' % tokens)
+    else:
+      # False means this was a non-NEWLINE group. Create a single raw sentence
+      # out of it.
+      raw_sentences.append(tokens)
 
   # Now we append a NEWLINE token after all sentences. Note that our tokenizer
   # drops any trailing \n's before tokenizing, but for the purpose of forming
@@ -105,37 +112,3 @@ def code_to_cubert_sentences(
                subtokenized_sentences)
 
   return subtokenized_sentences
-
-
-def main(argv):
-  if len(argv) > 1:
-    raise app.UsageError('Too many command-line arguments.')
-
-  # The value of the `TokenizerEnum` is a `CuBertTokenizer` subclass.
-  tokenizer = FLAGS.tokenizer.value()
-  subword_tokenizer = text_encoder.SubwordTextEncoder(FLAGS.vocabulary_filepath)
-
-  with open(FLAGS.input_filepath, 'r') as input_file:
-    code = input_file.read()
-    print('#' * 80)
-    print('Original Code')
-    print('#' * 80)
-    print(code)
-
-  subtokenized_sentences = code_to_cubert_sentences(
-      code=code,
-      initial_tokenizer=tokenizer,
-      subword_tokenizer=subword_tokenizer)
-  print('#' * 80)
-  print('CuBERT Sentences')
-  print('#' * 80)
-  print(subtokenized_sentences)
-
-  with open(FLAGS.output_filepath, 'wt') as output_file:
-    output_file.write(json.dumps(subtokenized_sentences, indent=2))
-
-if __name__ == '__main__':
-  flags.mark_flag_as_required('vocabulary_filepath')
-  flags.mark_flag_as_required('input_filepath')
-  flags.mark_flag_as_required('output_filepath')
-  app.run(main)
---
+++ cubert/code_to_subtokenized_sentences_main.py
@@ -0,0 +1,78 @@
+# coding=utf-8
+# Copyright 2021 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This modules demonstrates how to convert code to subtokenized sentences."""
+import json
+
+
+from absl import app
+from absl import flags
+from tensor2tensor.data_generators import text_encoder
+
+
+from cubert import code_to_subtokenized_sentences
+from cubert import tokenizer_registry
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('vocabulary_filepath', None,
+                    'Path to the subword vocabulary.')
+
+flags.DEFINE_string('input_filepath', None,
+                    'Path to the Python source code file.')
+
+flags.DEFINE_string('output_filepath', None,
+                    'Path to the output file of subtokenized source code.')
+
+flags.DEFINE_enum_class(
+    'tokenizer',
+    default=tokenizer_registry.TokenizerEnum.PYTHON,
+    enum_class=tokenizer_registry.TokenizerEnum,
+    help='The tokenizer to use.')
+
+
+def main(argv):
+  if len(argv) > 1:
+    raise app.UsageError('Too many command-line arguments.')
+
+  # The value of the `TokenizerEnum` is a `CuBertTokenizer` subclass.
+  tokenizer = FLAGS.tokenizer.value()
+  subword_tokenizer = text_encoder.SubwordTextEncoder(FLAGS.vocabulary_filepath)
+
+  with open(FLAGS.input_filepath, 'r') as input_file:
+    code = input_file.read()
+    print('#' * 80)
+    print('Original Code')
+    print('#' * 80)
+    print(code)
+
+  subtokenized_sentences = (
+      code_to_subtokenized_sentences.code_to_cubert_sentences(
+          code=code,
+          initial_tokenizer=tokenizer,
+          subword_tokenizer=subword_tokenizer))
+  print('#' * 80)
+  print('CuBERT Sentences')
+  print('#' * 80)
+  print(subtokenized_sentences)
+
+  with open(FLAGS.output_filepath, 'wt') as output_file:
+    output_file.write(json.dumps(subtokenized_sentences, indent=2))
+
+if __name__ == '__main__':
+  flags.mark_flag_as_required('vocabulary_filepath')
+  flags.mark_flag_as_required('input_filepath')
+  flags.mark_flag_as_required('output_filepath')
+  app.run(main)
