Fixing numerical instability in positional encoding on TPU by reverting posenc code from a matmul approach (which is apparently unstable) to element-wise multiplication.
PiperOrigin-RevId: 354710397

jaxnerf/nerf/model_utils.py
@@ -133,29 +133,11 @@ def sample_along_rays(key, origins, directions, num_samples, near, far,
   return z_vals, coords
 
 
-def generate_posenc_basis(deg, num_dims):
-  """Generates a posenc "basis" of degree `deg` for `num_dims` dimensions."""
-  return jnp.concatenate([2**i * jnp.eye(num_dims) for i in range(deg)], 1)
-
-
-def encode_coordinate(x, basis, legacy_posenc_order=False):
-  """Concatenate `x` with Fourier features of `x` projected onto `basis`."""
-  xb = x @ basis
-  # Instead of computing [sin(x), cos(x)], we use the trig identity
-  # cos(x) = sin(x + pi/2) and do one vectorized call to sin([x, x+pi/2]).
-  four_feat = jnp.sin(jnp.concatenate([xb, xb + 0.5 * jnp.pi], axis=-1))
-  # TODO(bydeng): remove this re-ordering when a new batch of pre-trained
-  # models is ready.
-  if legacy_posenc_order:
-    four_feat = jnp.moveaxis(
-        jnp.reshape(four_feat,
-                    list(four_feat.shape[:-1]) + [2, -1, x.shape[-1]]), -3, -2)
-    four_feat = jnp.reshape(four_feat, list(four_feat.shape[:-3]) + [-1])
-  return jnp.concatenate([x] + [four_feat], axis=-1)
-
-
 def posenc(x, deg, legacy_posenc_order=False):
   """Concatenate `x` with a positional encoding of `x` with degree `deg`.
+
+  Instead of computing [sin(x), cos(x)], we use the trig identity
+  cos(x) = sin(x + pi/2) and do one vectorized call to sin([x, x+pi/2]).
 
   Args:
     x: jnp.ndarray, variables to be encoded. Note that x should be in [-pi, pi].
@@ -165,8 +147,19 @@ def posenc(x, deg, legacy_posenc_order=False):
   Returns:
     encoded: jnp.ndarray, encoded variables.
   """
-  return encode_coordinate(x, generate_posenc_basis(deg, x.shape[-1]),
-                           legacy_posenc_order)
+  if deg == 0:
+    return x
+  scales = jnp.array([2**i for i in range(deg)])
+  if legacy_posenc_order:
+    xb = x[Ellipsis, None, :] * scales[:, None]
+    four_feat = jnp.reshape(
+        jnp.sin(jnp.stack([xb, xb + 0.5 * jnp.pi], -2)),
+        list(x.shape[:-1]) + [-1])
+  else:
+    xb = jnp.reshape((x[Ellipsis, None, :] * scales[:, None]),
+                     list(x.shape[:-1]) + [-1])
+    four_feat = jnp.sin(jnp.concatenate([xb, xb + 0.5 * jnp.pi], axis=-1))
+  return jnp.concatenate([x] + [four_feat], axis=-1)
 
 
 def volumetric_rendering(rgb, sigma, z_vals, dirs, white_bkgd):