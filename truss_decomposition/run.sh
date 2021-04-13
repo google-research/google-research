9eaaa0ae73d8e64c0456f780832e14a713bd184c
Luca Versari <veluca@google.com>
2021-04-13 08:49:54 -0700

    [truss_decomposition] Use sudo to install gflags
    PiperOrigin-RevId: 368223305

--- truss_decomposition/run.sh
+++ truss_decomposition/run.sh
@@ -12,10 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
 
 # Install gflags development files if not already present.
-[ -d /usr/include/gflags/ ] || apt install libgflags-dev
+[ -d /usr/include/gflags/ ] || sudo apt -y install libgflags-dev
 
 cd "$(readlink -f "$(dirname "$0")")"
 
