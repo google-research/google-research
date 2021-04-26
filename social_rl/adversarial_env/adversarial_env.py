fc19573ac0ad92fe6be5d5b48d753a3cd86074ee
Natasha Jaques <natashajaques@google.com>
2021-04-26 10:59:37 -0700

    Fix multiagent env to handle variable name change in underlying tf-agents class.
    PiperOrigin-RevId: 370496072

--- social_rl/adversarial_env/adversarial_env.py
+++ social_rl/adversarial_env/adversarial_env.py
@@ -182,7 +182,7 @@ class AdversarialGymWrapper(gym_wrapper.GymWrapper):
 
   def _step(self, action):
     # Automatically reset the environments on step if they need to be reset.
-    if self._auto_reset and self._done:
+    if self._handle_auto_reset and self._done:
       return self.reset_agent()
 
     action = action.item() if self._action_is_discrete else action
