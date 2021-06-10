a3d20dd6f1648e89d24cc9603d86c3f8ebd8d634
Joel Shor <joelshor@google.com>
2021-06-10 02:25:57 -0700

    Fix `precomputed_features` codepath.
    The fix involves combining as much of the codepath between precompute and non-precomputed as possible.
    
    PiperOrigin-RevId: 378606011

--- non_semantic_speech_benchmark/distillation/eval_keras.py
+++ non_semantic_speech_benchmark/distillation/eval_keras.py
@@ -37,12 +37,14 @@ flags.DEFINE_integer('ml', 16000, 'Minimum length.')
 flags.DEFINE_alias('min_length', 'ml')
 
 flags.DEFINE_boolean(
-    'precomputed_frontend_and_targets', False,
-    'Flag to enable training with precomputed frontend and targets. '
-    'If True, `file_pattern` must point to tf_records of tf.Examples. '
-    'See get_data.get_precomputed_data for details about tf.Example formatting. '
-    'If True, `teacher_model_hub`, `output_key`, `samples_key` '
-    'and `min_length` flags are ignored.')
+    'precomputed_targets', False,
+    'Flag to enable training with precomputed targets. '
+    'If True, `file_pattern` must point to precomputed targets, and '
+    '`target_key` must be supplied.')
+flags.DEFINE_string(
+    'target_key', None, 'Teacher embedding key in precomputed tf.Examples. '
+    'This flag is ignored if `precomputed_targets` is False.')
+
 
 # Teacher / student network flags.
 flags.DEFINE_string('teacher_model_hub', None, 'Hub teacher model.')
@@ -95,7 +97,7 @@ def eval_and_report():
       output_dimension=FLAGS.output_dimension,
       alpha=FLAGS.alpha,
       mobilenet_size=FLAGS.mobilenet_size,
-      frontend=not FLAGS.precomputed_frontend_and_targets,
+      frontend=True,
       avg_pool=FLAGS.average_pool)
   checkpoint = tf.train.Checkpoint(model=model)
 
@@ -110,17 +112,26 @@ def eval_and_report():
     logging.info('Loaded weights for eval step: %s.', step)
 
     reader = tf.data.TFRecordDataset
+    target_key = FLAGS.target_key
+    if FLAGS.precomputed_targets:
+      teacher_fn = None
+      assert target_key is not None
+    else:
+      teacher_fn = get_data.savedmodel_to_func(
+          hub.load(FLAGS.teacher_model_hub), FLAGS.output_key)
+      assert target_key is None
     ds = get_data.get_data(
         file_pattern=FLAGS.file_pattern,
-        teacher_fn=get_data.savedmodel_to_func(
-            hub.load(FLAGS.teacher_model_hub), FLAGS.output_key),
         output_dimension=FLAGS.output_dimension,
         reader=reader,
         samples_key=FLAGS.samples_key,
         min_length=FLAGS.min_length,
-        batch_size=FLAGS.batch_size,
+        batch_size=FLAGS.train_batch_size,
         loop_forever=False,
-        shuffle=False)
+        shuffle=False,
+        teacher_fn=teacher_fn,
+        target_key=target_key,
+        shuffle_buffer_size=FLAGS.shuffle_buffer_size)
     logging.info('Got dataset for eval step: %s.', step)
     if FLAGS.take_fixed_data:
       ds = ds.take(FLAGS.take_fixed_data)
--- non_semantic_speech_benchmark/distillation/get_data.py
+++ non_semantic_speech_benchmark/distillation/get_data.py
@@ -20,136 +20,138 @@ import tensorflow as tf
 from non_semantic_speech_benchmark import file_utils
 
 
-def get_data(
-    file_pattern,
-    teacher_fn,
-    output_dimension,
-    reader,
-    samples_key,
-    min_length,
-    batch_size,
-    loop_forever,
-    shuffle,
-    shuffle_buffer_size=10000):
-  """Gets the data for TRILL distillation.
+SAMPLES_ = 'samples_'
+TARGETS_ = 'targets_'
 
-  This function is *always* stochastic.
+
+def get_data(file_pattern,
+             output_dimension,
+             reader,
+             samples_key,
+             min_length,
+             batch_size,
+             loop_forever,
+             shuffle,
+             teacher_fn=None,
+             target_key=None,
+             shuffle_buffer_size=10000):
+  """Gets data for TRILL distillation from a teacher or precomputed values.
 
   Args:
     file_pattern: Glob for input data.
-    teacher_fn: A function that takes 1 argument and returns label embeddings.
     output_dimension: Feature dimension of teacher output.
     reader: Class used to parse data on disk.
-    samples_key: Name of audio samples in tf.Examples
+    samples_key: Name of audio samples in tf.Examples.
     min_length: The minimum audio length. Should take sample rate into account.
       Examples smaller than this are dropped. Examples longer than this are
-      randomly cropped to this size.
+      randomly cropped to this size. If we are using precomputed targets,
+      drop examples that aren't equal to this.
     batch_size: Batch size of data in returned tf.data.Dataset.
     loop_forever: Python bool. Whether to loop forever.
     shuffle: Python bool. Whether to shuffle data.
-    shuffle_buffer_size: Size of shuffle buffer
-
+    teacher_fn: Optional. A function that takes 1 argument and returns label
+      embeddings. If `None`, get precomputed data from disk. If present, run
+      teacher function as part of the input pipeline. If `teacher_fn` is `None`,
+      `target_key` must be not None.
+    target_key: Required if reading precomputed features. Location of the target
+      embeddings.
+    shuffle_buffer_size: Size of shuffle buffer.
   Returns:
     A tf.data.Dataset of (audio samples, regression targets).
   """
   assert file_utils.Glob(file_pattern), file_pattern
-  assert callable(teacher_fn)
 
-  # Audio samples are variable length.
-  features = {
-      samples_key: tf.io.VarLenFeature(dtype=tf.float32),
-  }
-
-  # Load data into a dataset of batch size 1. Then preprocess.
-  ds = tf.data.experimental.make_batched_features_dataset(
-      file_pattern=file_pattern,
-      batch_size=1,
-      num_epochs=None if loop_forever else 1,
-      reader_num_threads=tf.data.experimental.AUTOTUNE,
-      parser_num_threads=tf.data.experimental.AUTOTUNE,
-      features=features,
-      reader=reader,
-      shuffle=shuffle,
-      shuffle_buffer_size=shuffle_buffer_size,
-      prefetch_buffer_size=tf.data.experimental.AUTOTUNE,
-      sloppy_ordering=True)
-
-  ds = tf_data_pipeline(ds, teacher_fn, samples_key, min_length, batch_size,
-                        output_dimension)
-
-  return ds
-
-
-def get_precomputed_data(file_pattern, output_dimension, frontend_key,
-                         target_key, batch_size, num_epochs,
-                         shuffle_buffer_size):
-  """Gets precomputed frontend features and targets for TRILL distillation.
-
-  This function is *always* stochastic.
-  Args:
-    file_pattern: Glob for input data.
-    output_dimension: Feature dimension of teacher output.
-    frontend_key: Name of frontend features in tf.Examples.
-    target_key: Name of targets (i.e. teacher model outputs) in tf.Examples.
-    batch_size: Batch size of data in returned tf.data.Dataset.
-    num_epochs: Number of training epochs.
-    shuffle_buffer_size: Size of dataset shuffle buffer.
-
-  Returns:
-    A tf.data.Dataset of (frontend features, regression targets).
-  """
-
-  def _parse_examples(record):
-    feature_description = {
-        frontend_key: tf.io.FixedLenFeature([int(96 * 64)], tf.float32),
+  if teacher_fn is None:
+    assert target_key
+    # Use precomputed targets. We trust the data generation process to create
+    # inputs of the right size, so use fixed-length input for samples.
+    features = {
+        samples_key: tf.io.FixedLenFeature([min_length], tf.float32),
         target_key: tf.io.FixedLenFeature([output_dimension], tf.float32),
     }
-    example = tf.io.parse_example(record, feature_description)
-    return tf.reshape(example['audio'], (96, 64)), example['label']
+    cur_batch_size = batch_size
+    def _rename_dict(kv):
+      return {SAMPLES_: kv[samples_key], TARGETS_: kv[target_key]}
+  else:
+    assert target_key is None
+    features = {
+        samples_key: tf.io.VarLenFeature(dtype=tf.float32),
+    }
+    cur_batch_size = 1
+    def _rename_dict(kv):
+      return {SAMPLES_: kv[samples_key]}
 
-  options = tf.data.Options()
-  options.experimental_deterministic = False
-  files_ds = tf.data.Dataset.list_files(file_pattern).with_options(options)
-  return tf.data.TFRecordDataset(files_ds)\
-    .map(_parse_examples)\
-    .batch(batch_size, drop_remainder=True)\
-    .shuffle(shuffle_buffer_size, reshuffle_each_iteration=True)\
-    .repeat(num_epochs)\
-    .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+  # Load data into a dataset of batch size 1, then preprocess if necessary.
+  ds = (
+      tf.data.experimental.make_batched_features_dataset(
+          file_pattern=file_pattern,
+          batch_size=cur_batch_size,
+          num_epochs=None if loop_forever else 1,
+          reader_num_threads=tf.data.experimental.AUTOTUNE,
+          parser_num_threads=tf.data.experimental.AUTOTUNE,
+          features=features,
+          reader=reader,
+          shuffle=shuffle,
+          shuffle_buffer_size=shuffle_buffer_size,
+          prefetch_buffer_size=tf.data.experimental.AUTOTUNE,
+          sloppy_ordering=True)
+      .map(_rename_dict, num_parallel_calls=tf.data.experimental.AUTOTUNE))
 
+  if teacher_fn is not None:
+    # Create target embeddings from `teacher_fn`.
+    assert callable(teacher_fn)
+    @tf.function
+    def _audio_to_embeddings(samples):
+      return _audio_to_embeddings_fn(samples, teacher_fn, output_dimension)
+    ds = (ds
+          .filter(lambda kv: _filter_fn(kv, min_length))
+          .map(lambda kv: _crop_fn(kv, min_length),
+               num_parallel_calls=tf.data.experimental.AUTOTUNE)
+          .batch(batch_size)
+          .map(_audio_to_embeddings,
+               num_parallel_calls=tf.data.experimental.AUTOTUNE))
 
-def tf_data_pipeline(ds, teacher_fn, samples_key, min_length, batch_size,
-                     output_dimension):
-  """Create tf.data pipeline for reading data."""
-  # Filter examples that are too short, crop, map labels to integers, and batch.
-  @tf.function
-  def _filter_fn(kv):
-    d_shape = kv[samples_key].dense_shape
-    one = tf.cast(1, d_shape.dtype)
-    tf.debugging.assert_equal(d_shape[0], one)  # We expect batch size to be 1.
-    return d_shape[1] > min_length
-  def _crop(kv):
-    samples = kv[samples_key]
-    samples = tf.sparse.to_dense(samples)
-    samples.shape.assert_has_rank(2)
-    samples = tf.squeeze(samples, axis=0)
-    samples = tf.image.random_crop(samples, [min_length], seed=123, name='crop')
-    samples.set_shape([min_length])
-    return samples
-  def _audio_to_embeddings(samples):
-    teacher_embeddings = teacher_fn(samples)
-    teacher_embeddings.shape.assert_has_rank(2)
-    teacher_embeddings.set_shape([None, output_dimension])
-    return (samples, teacher_embeddings)
-  autotune_ = tf.data.experimental.AUTOTUNE
+  # Convert results to tuple.
   ds = (ds
-        .filter(_filter_fn)
-        .map(_crop, num_parallel_calls=autotune_)
-        .batch(batch_size)
-        .map(_audio_to_embeddings, num_parallel_calls=autotune_)
+        .map(lambda kv: (kv[SAMPLES_], kv[TARGETS_]),
+             num_parallel_calls=tf.data.experimental.AUTOTUNE)
         .prefetch(2))
 
+  assert len(ds.element_spec) == 2, ds.element_spec
+  ds.element_spec[0].shape.assert_has_rank(2)  # audio samples
+  ds.element_spec[1].shape.assert_has_rank(2)  # teacher embeddings
+  assert tuple(ds.element_spec[0].shape) == (None, min_length)
+  assert tuple(ds.element_spec[1].shape) == (None, output_dimension)
+
   return ds
+
+
+def _filter_fn(kv, min_length):
+  """Filter examples that are too short."""
+  d_shape = kv[SAMPLES_].dense_shape
+  one = tf.cast(1, d_shape.dtype)
+  tf.debugging.assert_equal(d_shape[0], one)  # We expect batch size to be 1.
+  return d_shape[1] > min_length
+
+
+def _crop_fn(kv, min_length):
+  """Crop audio to expected length."""
+  samples = kv[SAMPLES_]
+  samples = tf.sparse.to_dense(samples)
+  samples.shape.assert_has_rank(2)
+  samples = tf.squeeze(samples, axis=0)
+  samples = tf.image.random_crop(samples, [min_length], seed=123, name='crop')
+  samples.set_shape([min_length])
+  return {SAMPLES_: samples}
+
+
+def _audio_to_embeddings_fn(kv, teacher_fn, output_dimension):
+  """Map audio to teacher labels."""
+  samples = kv[SAMPLES_]
+  teacher_embeddings = teacher_fn(samples)
+  teacher_embeddings.shape.assert_has_rank(2)
+  teacher_embeddings.set_shape([None, output_dimension])
+  return {SAMPLES_: samples, TARGETS_: teacher_embeddings}
 
 
 def savedmodel_to_func(saved_model, output_key, sample_rate=16000):
