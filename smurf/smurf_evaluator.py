7d93fd657bf1dc041b1f526159c26a503c1b9b77
slowy07 <slowy.arfy@gmail.com>
2021-07-13 07:23:39 +0700

    [fixed] fixed indent and change return to pass because syntax error

--- smurf/smurf_evaluator.py
+++ smurf/smurf_evaluator.py
@@ -25,60 +25,61 @@ import gin
 import tensorflow as tf
 
 
-  candidate_checkpoint = None
-  smurf =build_network(batch_size=1)
+candidate_checkpoint = None
+smurf =build_network(batch_size=1)
 
-  weights = {
-      'census': FLAGS.weight_census,
-      'smooth1': FLAGS.weight_smooth1,
-      'smooth2': FLAGS.weight_smooth2,
-  }
-  evaluate_fn, _ = smurf_data.make_eval_function(
-      FLAGS.eval_on,
-      FLAGS.height,
-      FLAGS.width,
-      progress_bar=True,
-      plot_dir=FLAGS.plot_dir,
-      num_plots=50,
-      weights=weights)
+weights = {
+    'census': FLAGS.weight_census,
+    'smooth1': FLAGS.weight_smooth1,
+    'smooth2': FLAGS.weight_smooth2,
+}
+evaluate_fn, _ = smurf_data.make_eval_function(
+    FLAGS.eval_on,
+    FLAGS.height,
+    FLAGS.width,
+    progress_bar=True,
+    plot_dir=FLAGS.plot_dir,
+    num_plots=50,
+    weights=weights)
 
-  latest_checkpoint = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
-  while 1:
-    # Wait for a new checkpoint
-    while candidate_checkpoint == latest_checkpoint:
-      logging.log_every_n(logging.INFO,
-                          'Waiting for a new checkpoint, at %s, latest is %s',
-                          3, FLAGS.checkpoint_dir, latest_checkpoint)
-      time.sleep(45.)
-      candidate_checkpoint = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+latest_checkpoint = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+while 1:
+  # Wait for a new checkpoint
+  while candidate_checkpoint == latest_checkpoint:
+    logging.log_every_n(logging.INFO,
+                        'Waiting for a new checkpoint, at %s, latest is %s',
+                        3, FLAGS.checkpoint_dir, latest_checkpoint)
+    time.sleep(45.)
     candidate_checkpoint = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
-    latest_checkpoint = candidate_checkpoint
-    logging.info('New checkpoint found: %s', candidate_checkpoint)
-    # This forces the checkpoint manager to reexamine the checkpoint directory
-    # and become aware of the new checkpoint.
-    smurf.update_checkpoint_dir(FLAGS.checkpoint_dir)
-    smurf.restore()
+  candidate_checkpoint = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+  latest_checkpoint = candidate_checkpoint
+  logging.info('New checkpoint found: %s', candidate_checkpoint)
+  # This forces the checkpoint manager to reexamine the checkpoint directory
+  # and become aware of the new checkpoint.
+  smurf.update_checkpoint_dir(FLAGS.checkpoint_dir)
+  smurf.restore()
 
-    step = tf.compat.v1.train.get_global_step().numpy()
-    terminate = False
-    if step >= FLAGS.num_train_steps:
-      # If initializing from another checkpoint directory, the first checkpoint
-      # will be the init checkpoint and might have steps > num_train_steps.
-      # Don't quit in this case.
-      terminate = True
-      if FLAGS.init_checkpoint_dir:
-        with gfile.Open(os.path.join(FLAGS.checkpoint_dir, 'checkpoint'),
-                        'r') as f:
-          if len(f.readlines()) == 2:
-            logging.info('Continuing evaluation after evaluating '
-                         'init_checkpoint.')
-            terminate = False
+  step = tf.compat.v1.train.get_global_step().numpy()
+  terminate = False
+  if step >= FLAGS.num_train_steps:
+    # If initializing from another checkpoint directory, the first checkpoint
+    # will be the init checkpoint and might have steps > num_train_steps.
+    # Don't quit in this case.
+    terminate = True
+    if FLAGS.init_checkpoint_dir:
+      with gfile.Open(os.path.join(FLAGS.checkpoint_dir, 'checkpoint'),
+                      'r') as f:
+        if len(f.readlines()) == 2:
+          logging.info('Continuing evaluation after evaluating '
+                        'init_checkpoint.')
+          terminate = False
 
-    eval_results = evaluate_fn(smurf)
-    smurf_plotting.print_eval(eval_results)
+  eval_results = evaluate_fn(smurf)
+  smurf_plotting.print_eval(eval_results)
 
-    if terminate or FLAGS.run_eval_once:
-      return
+  if terminate or FLAGS.run_eval_once:
+    pass
+    # return
 
 
 def main(unused_argv):
