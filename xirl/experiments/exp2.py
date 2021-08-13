39e0d1b864998e6e67c6e5cba738723df0b21d0e
Kevin <kevinarmandzakka@gmail.com>
2021-08-12 17:09:07 -0700

    Same fixes for pretraining exp2.

--- xirl/experiments/exp2.py
+++ xirl/experiments/exp2.py
@@ -24,16 +24,17 @@ from absl import flags
 # The supported pretraining algorithms.
 ALGORITHMS = ["xirl", "tcn", "lifs", "goal_classifier", "raw_imagenet"]
 # The embodiment classes in the dataset.
-EMBODIMENTS = ["gripper", "shortstick", "mediumstick", "longstick"]
+EMBODIMENTS = ["longstick", "mediumstick", "shortstick", "gripper"]
 # Mapping from pretraining algorithm to config file.
 ALGO_TO_CONFIG = {
     "xirl": "configs/pretraining/tcc.py",
     "lifs": "configs/pretraining/lifs.py",
     "tcn": "configs/pretraining/tcn.py",
     "goal_classifier": "configs/pretraining/classifier.py",
+    "raw_imagenet": "configs/pretraining/imagenet.py",
 }
 # We want to pretrain on the entire 1k demonstrations.
-MAX_DEMONSTRATIONS = 1_000
+MAX_DEMONSTRATIONS = -1
 
 FLAGS = flags.FLAGS
 flags.DEFINE_enum("algo", None, ALGORITHMS, "The pretraining algorithm to use.")
@@ -43,31 +44,28 @@ flags.mark_flag_as_required("algo")
 def main(_):
   for embodiment in EMBODIMENTS:
     # Generate a unique experiment name.
-    experiment_name = "exp2_embodiment={}_maxdemos={}_uid={}".format(
-        embodiment, MAX_DEMONSTRATIONS, int(random.random() * 1e9))
+    experiment_name = "exp2_algo={}_embodiment={}_maxdemos={}_uid={}".format(
+        FLAGS.algo, embodiment, MAX_DEMONSTRATIONS, int(random.random() * 1e9))
     print(f"Experiment name: {experiment_name}")
 
     # Train on all classes but the given embodiment.
     trainable_embs = tuple(set(EMBODIMENTS) - set([embodiment]))
 
-    # The 'raw_imagenet' baseline does not need to do any pretraining.
-    if FLAGS.algo != "raw_imagenet":
-      subprocess.call([
-          "python",
-          "pretrain.py",
-          "--experiment_name",
-          experiment_name,
-          "--config",
-          f"{ALGO_TO_CONFIG[FLAGS.algo]}",
-          "--config.DATA.PRETRAIN_ACTION_CLASS",
-          f"{repr(trainable_embs)}",
-          # For the downstream action classes, we'll load all embodiments to
-          # monitor performance on the unseen embodiment as well.
-          "--config.DATA.DOWNSTREAM_ACTION_CLASS",
-          "()",
-          "--config.DATA.MAX_VIDS_PER_CLASS",
-          f"{MAX_DEMONSTRATIONS}",
-      ])
+    subprocess.call([
+        "python",
+        "pretrain.py",
+        "--experiment_name",
+        experiment_name,
+        "--raw_imagenet" if FLAGS.algo == "raw_imagenet" else '',
+        "--config",
+        f"{ALGO_TO_CONFIG[FLAGS.algo]}",
+        "--config.DATA.PRETRAIN_ACTION_CLASS",
+        f"{repr(trainable_embs)}",
+        "--config.DATA.DOWNSTREAM_ACTION_CLASS",
+        f"{repr(trainable_embs)}",
+        "--config.DATA.MAX_VIDS_PER_CLASS",
+        f"{MAX_DEMONSTRATIONS}",
+    ])
 
     # The 'goal_classifier' baseline does not need to compute a goal embedding.
     if FLAGS.algo != "goal_classifier":
