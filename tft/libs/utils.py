e12202b2e62fc76c93ae1e56029b21d22166cb51
Kanakorn Horsiritham <kanakorn.h@psu.ac.th>
2021-10-22 22:03:51 +0700

    Update utils.py

--- tft/libs/utils.py
+++ tft/libs/utils.py
@@ -169,10 +169,10 @@ def save(tf_session, model_folder, cp_name, scope=None):
   """
   # Save model
   if scope is None:
-    saver = tf.train.Saver()
+    saver = tf.compat.v1.train.Saver()
   else:
-    var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope)
-    saver = tf.train.Saver(var_list=var_list, max_to_keep=100000)
+    var_list = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.TRAINABLE_VARIABLES, scope=scope)
+    saver = tf.compat.v1.train.Saver(var_list=var_list, max_to_keep=100000)
 
   save_path = saver.save(tf_session,
                          os.path.join(model_folder, '{0}.ckpt'.format(cp_name)))
@@ -197,14 +197,14 @@ def load(tf_session, model_folder, cp_name, scope=None, verbose=False):
   print_weights_in_checkpoint(model_folder, cp_name)
 
   initial_vars = set(
-      [v.name for v in tf.get_default_graph().as_graph_def().node])
+      [v.name for v in tf.compat.v1.get_default_graph().as_graph_def().node])
 
   # Saver
   if scope is None:
-    saver = tf.train.Saver()
+    saver = tf.compat.v1.train.Saver()
   else:
-    var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=scope)
-    saver = tf.train.Saver(var_list=var_list, max_to_keep=100000)
+    var_list = tf.compat.v1.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=scope)
+    saver = tf.compat.v1.train.Saver(var_list=var_list, max_to_keep=100000)
   # Load
   saver.restore(tf_session, load_path)
   all_vars = set([v.name for v in tf.get_default_graph().as_graph_def().node])
