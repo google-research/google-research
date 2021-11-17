Minor fixes to the distance computation for relative attention

latent_programmer/decomposition_transformer_attention/decomposition_models.py
@@ -97,7 +97,7 @@ class DecomposeAttentionTransformer(nn.Module):
                       self.has_variable('relative_attention', 'max_io_distance') and
                       self.has_variable('relative_attention', 'max_program_distance')
                       )
-    if is_initialized and cfg.clip_relative_attention:
+    if is_initialized and self.config.clip_relative_attention:
       max_input_distance = self.max_input_distance.item()
       max_output_distance = self.max_output_distance.item()
       max_io_distance = self.max_io_distance.item()
@@ -124,13 +124,13 @@ class DecomposeAttentionTransformer(nn.Module):
         config=base_decoder_config, name='decoder')
 
     self.max_input_distance = self.variable(
-        'relative_attention', 'max_input_distance', jnp.zeros, [])
+        'relative_attention', 'max_input_distance', lambda: jnp.zeros((), jnp.int32))
     self.max_output_distance = self.variable(
-        'relative_attention', 'max_output_distance', jnp.zeros, [])
+        'relative_attention', 'max_output_distance', lambda: jnp.zeros((), jnp.int32))
     self.max_io_distance = self.variable(
-        'relative_attention', 'max_program_distance', jnp.zeros, [])
+        'relative_attention', 'max_io_distance', lambda: jnp.zeros((), jnp.int32))
     self.max_program_distance = self.variable(
-        'relative_attention', 'max_io_distance', jnp.zeros, [])
+        'relative_attention', 'max_program_distance', lambda: jnp.zeros((), jnp.int32))
 
   def encode(self,
              inputs,
@@ -259,17 +259,21 @@ class DecomposeAttentionTransformer(nn.Module):
     encoded_padding_mask = jnp.where(outputs > 0, 1, 0).astype(jnp.float32)
 
     if self.has_variable('relative_attention', 'max_input_distance'):
-      max_input_distance = jnp.max(jnp.count_nonzero(inputs, axis=0))
-      self.max_input_distance = max(self.max_input_distance, max_input_distance)
+      max_input_distance = jnp.max(jnp.count_nonzero(inputs == 0, axis=0))
+      self.max_input_distance.value = jnp.maximum(
+        self.max_input_distance.value, max_input_distance)
     if self.has_variable('relative_attention', 'max_input_distance'):
-      max_output_distance = jnp.max(jnp.count_nonzero(outputs, axis=0))
-      self.max_output_distance = max(self.max_output_distance, max_output_distance)
+      max_output_distance = jnp.max(jnp.count_nonzero(outputs == 0, axis=0))
+      self.max_output_distance.value = jnp.maximum(
+        self.max_output_distance.value, max_output_distance)
     if self.has_variable('relative_attention', 'max_io_distance'):
       max_io_distance = jnp.max(
-          jnp.count_nonzero(jnp.concatenate((inputs, outputs), axis=0), axis=0))
-      self.max_io_distance = max(self.max_io_distnace, max_io_distance)
+          jnp.count_nonzero(jnp.concatenate((inputs, outputs), axis=0) == 0, axis=0))
+      self.max_io_distance.value = jnp.maximum(
+        self.max_io_distance.value, max_io_distance)
     if self.has_variable('relative_attention', 'max_program_distance'):
-      max_program_distance = jnp.max(jnp.count_nonzero(programs, axis=0))
-      self.max_program_distance = max(self.max_program_distance, max_program_distance)
+      max_program_distance = jnp.max(jnp.count_nonzero(programs == 0, axis=0))
+      self.max_program_distance.value = jnp.maximum(
+        self.max_program_distance.value, max_program_distance)
 
     return self.decode(programs, encoded, encoded_padding_mask)

latent_programmer/decomposition_transformer_attention/train.py
@@ -20,17 +20,20 @@
 # pytype: disable=attribute-error
 
 import collections
+import dataclasses
 import functools
 import json
 import os
 import random
 import sys
+sys.path.append('../../')
 import time
 import typing
 
 from absl import app
 from absl import flags
 from absl import logging
+import flax
 from flax import jax_utils
 from flax import linen as nn
 from flax import optim
@@ -49,7 +52,6 @@ from latent_programmer.decomposition_transformer_attention import input_pipeline
 from latent_programmer.tasks.robust_fill import dsl
 from latent_programmer.tasks.robust_fill import tokens as dsl_tokens
 
-sys.path.append('../../')
 gfile = tf.io.gfile
 
 FLAGS = flags.FLAGS
@@ -95,7 +97,7 @@ flags.DEFINE_integer('num_relative_position_buckets', 32,
 flags.DEFINE_bool('bos_special_attention', False,
                   'Whether to use special relative attention computation for '
                   'BOS tokens.')
-flags.DEFINE_bool('clip_relative_attention', False,
+flags.DEFINE_bool('clip_relative_attention', True,
                   'Whether to clip relative positions during evaluation.')
 
 
@@ -282,7 +284,7 @@ def train_step(state,
   new_state = state.replace(
     step=step + 1,
     optimizer=new_optimizer,
-    ra_stats=jax.lax.pmean(new_ra_stats, 'batch'))
+    ra_stats=jax.lax.pmean(ra_stats, 'batch'))
   return new_state, metrics, new_dropout_rng
 
 