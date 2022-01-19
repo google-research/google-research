ee07902ea62e4a32c756baeff014bf8f3376ff0c
Peter Hedman <hedman@google.com>
2022-01-19 10:20:45 -0800

    Create loading indicator for the SNeRG viewer.
    PiperOrigin-RevId: 422845933

--- snerg/viewer/index.js
+++ snerg/viewer/index.js
@@ -791,6 +791,9 @@ function addHandlers() {
 function hideLoading() {
   let loading = document.getElementById('Loading');
   loading.style.display = 'none';
+
+  let loadingContainer = document.getElementById('loading-container');
+  loadingContainer.style.display = 'none';
 }
 
 /**
@@ -1269,6 +1272,8 @@ function loadOnFirstFrame() {
   // Also early out if the renderer is not supported.
   if (isRendererUnsupported()) {
     gSceneParams['loadingTextures'] = true;
+    let loadingContainer = document.getElementById('loading-container');
+    loadingContainer.style.display = 'none';
     return;
   }
 
