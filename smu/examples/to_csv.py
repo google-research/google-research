caf8c6be5a601aa8515200c33d17e4219bf6c929
Patrick Riley <patriley@gmail.com>
2022-01-20 14:45:11 -0500

    SMU: fix up to_csv.py

--- smu/examples/to_csv.py
+++ smu/examples/to_csv.py
@@ -2,48 +2,51 @@
 # by iterating over the conformers
 
 import csv
+import sys
 
 from smu import smu_sqlite
 
 db = smu_sqlite.SMUSQLite('20220104_standard.sqlite', 'r')
 
-OUTFILE = '/tmp/smu_example_csv.csv'
-with open(OUTFILE, 'w', newline='') as outfile:
-    writer = csv.writer(outfile)
+print('# This is example csv output from smu/examples/to_csv.py')
+print('# Please see the comments in that file for explanation')
 
-    # This is where you write the header of the csv with whatever columns names
-    # you like.
-    writer.writerow(['conformer_id',
-                     'energy',
-                     'homo',
-                     'lumo',
-                     'first important frequency'])
+# For this example, we will write to standard out.
+# For a real application, you would want to use
+# open('path_to_my_file.csv', 'w')
+# instead of sys.stdout
+writer = csv.writer(sys.stdout)
 
-    count = 0
-    # This iteration will go through all conformers in the database.
-    for conformer in db:
+# This is where you write the header of the csv with whatever columns names
+# you like.
+writer.writerow(['conformer_id',
+                 'energy',
+                 'homo',
+                 'lumo',
+                 'first important frequency'])
 
-        # This is kind of a silly filter, but this shows how to filter
-        # for some conformers and not just the first couple.
-        if conformer.optimized_geometry.atom_positions[0].x > -3:
-            continue
+count = 0
+# This iteration will go through all conformers in the database.
+for conformer in db:
 
-        # This is where you would choose the fields to print.
-        # See field_access.py for more examples of accessing fields.
-        writer.writerow([
-            conformer.conformer_id,
-            conformer.properties.single_point_energy_atomic_b5.value,
-            conformer.properties.homo_pbe0_6_311gd.value,
-            conformer.properties.lumo_pbe0_6_311gd.value,
-            conformer.properties.harmonic_frequencies.value[6],
-        ])
+  # This is kind of a silly filter, but this shows how to filter
+  # for some conformers and not just the first couple.
+  if conformer.optimized_geometry.atom_positions[0].x > -3:
+    continue
 
-        # This breaks out of the loop after a couple of records just so this
-        # examples runs quickly. If you want process the whole dataset,
-        # remove this.
-        count += 1
-        if count == 5:
-            break
+  # This is where you would choose the fields to print.
+  # See field_access.py for more examples of accessing fields.
+  writer.writerow([
+    conformer.conformer_id,
+    conformer.properties.single_point_energy_atomic_b5.value,
+    conformer.properties.homo_pbe0_6_311gd.value,
+    conformer.properties.lumo_pbe0_6_311gd.value,
+    conformer.properties.harmonic_frequencies.value[6],
+  ])
 
-print('An example CSV has been written to', OUTFILE)
-print('Please see the comments in csv.py for description')
+  # This breaks out of the loop after a couple of records just so this
+  # examples runs quickly. If you want process the whole dataset,
+  # remove this.
+  count += 1
+  if count == 5:
+    break
