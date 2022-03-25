7e72b6161b83a451a0aec3e8785a6347706120be
Felipe Llinares-Lopez <fllinares@google.com>
2022-03-25 02:54:43 -0700

    Adds homology head to `DedalLight` TFHub-compatible interface.
    PiperOrigin-RevId: 437202476

--- dedal/models/dedal.py
+++ dedal/models/dedal.py
@@ -260,10 +260,12 @@ class Dedal(tf.keras.Model):
 class DedalLight(tf.keras.Model):
   """A light-weight model to be easily exported with tf.saved_model."""
 
-  def __init__(self, encoder, aligner, **kwargs):
+  def __init__(
+      self, encoder, aligner, homology_head, **kwargs):
     super().__init__(**kwargs)
     self.encoder = encoder
     self.aligner = aligner
+    self.homology_head = homology_head
 
   @tf.function
   def call(self, inputs, training = False, embeddings_only = False):
@@ -279,5 +281,16 @@ class DedalLight(tf.keras.Model):
     embedding_pairs, mask_pairs = pairs_lib.build(indices, embeddings, masks)
     alignments = self.aligner(
         embedding_pairs, mask=mask_pairs, training=training)
-    return alignments
 
+    # Computes homology scores from SW scores and sequence lengths.
+    homology_scores = self.homology_head(
+        alignments, mask=mask_pairs, training=training)
+    # Removes "dummy" trailing dimension.
+    homology_scores = tf.squeeze(homology_scores, axis=-1)
+
+    return {
+        'sw_params': alignments[2],
+        'sw_scores': alignments[0],
+        'paths': alignments[1],
+        'homology_logits': homology_scores,
+    }
