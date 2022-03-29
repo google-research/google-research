fe19cf7d36eb564b9515251820ec1816a3bc7e87
Tharun Sankar <tsankar@google.com>
2022-03-29 16:36:38 -0700

    Image/file utils for Next Day Wildfire Spread
    PiperOrigin-RevId: 438157151

---
+++ simulation_research/next_day_wildfire_spread/file_util.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+# Copyright 2022 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utilities for working with local files."""
+import os
+from typing import Text
+
+
+f_exists = os.path.exists
+f_makedirs = os.makedirs
+
+
+def maybe_make_dirs(path):
+  """Creates the sub directories leading up to the given file.
+
+  Args:
+    path: The path to a file (i.e., the last word in the path is assumed to be a
+      file, not a directory).
+
+  Returns:
+    The path generated.
+  """
+  dirname = os.path.dirname(path)
+  if dirname and not f_exists(dirname):
+    f_makedirs(dirname)
+  return dirname
