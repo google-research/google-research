6c51450861a7c7abbfc1537c907c822c7075e7b6
Yilei Yang <yileiyang@google.com>
2022-04-08 04:31:41 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 440331106

--- weak_disentangle/datasets.py
+++ weak_disentangle/datasets.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# python3
 """Dataset-specific utilities."""
 
 # pylint: disable=g-bad-import-order, unused-import, g-multiple-import
--- weak_disentangle/tensorsketch/normalization/running_normalization.py
+++ weak_disentangle/tensorsketch/normalization/running_normalization.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# python3
 """Running Normalization utilities."""
 
 # pylint: disable=g-bad-import-order, g-direct-tensorflow-import
