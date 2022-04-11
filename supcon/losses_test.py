6fb0566579626df378606dc5dbef615560f84f3d
Yilei Yang <yileiyang@google.com>
2022-04-11 07:01:59 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 440878507

--- supcon/losses_test.py
+++ supcon/losses_test.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Tests for supcon.losses."""
 from absl.testing import parameterized
 import numpy as np
--- supcon/resnet.py
+++ supcon/resnet.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """ResNet implementation."""
 
 import tensorflow.compat.v1 as tf
