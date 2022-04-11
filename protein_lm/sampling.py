ce8367fc228ba31215c96f48937fd2e3570f53c1
Yilei Yang <yileiyang@google.com>
2022-04-11 13:49:48 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 440986727

--- protein_lm/sampling.py
+++ protein_lm/sampling.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Functions related to sampling from a model."""
 
 from absl import logging
