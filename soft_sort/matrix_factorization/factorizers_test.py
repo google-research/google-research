4a0e5a2bf4ebe8e8880f416e36e2a9ed3f566992
Yilei Yang <yileiyang@google.com>
2022-04-11 17:13:04 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 441036333

--- soft_sort/matrix_factorization/factorizers_test.py
+++ soft_sort/matrix_factorization/factorizers_test.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Tests for soft sorting tensorflow layers."""
 
 from absl.testing import parameterized
--- soft_sort/matrix_factorization/training.py
+++ soft_sort/matrix_factorization/training.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Puts everything together to train any factorization on any dataset."""
 
 import os.path
