74ab5ec0766e918b5b5d3f8befa44d1d8c4d3f4a
Yilei Yang <yileiyang@google.com>
2022-04-12 18:44:45 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 441348142

--- rrlfd/residual/environment_loop.py
+++ rrlfd/residual/environment_loop.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# python3
 """Agent-environment training loop based on acme."""
 
 import collections
