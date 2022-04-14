16b0d9c8277c1f71d2b6f9890a3f25c53fff50a0
Yilei Yang <yileiyang@google.com>
2022-04-14 12:58:11 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 441832778

--- covid_epidemiology/src/models/encoders/variable_encoder_builder.py
+++ covid_epidemiology/src/models/encoders/variable_encoder_builder.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Constructs a variable encoder based on an encoder spec."""
 
 from typing import Dict, List, Optional
--- covid_epidemiology/src/models/encoders/variable_encoders.py
+++ covid_epidemiology/src/models/encoders/variable_encoders.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Encoders that use time series inputs to predict SEIR terms."""
 import abc
 import logging
