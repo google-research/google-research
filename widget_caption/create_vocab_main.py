24aef0886787862ad9091618c0a1f5499a847979
Yilei Yang <yileiyang@google.com>
2022-04-14 03:07:30 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 441711704

--- widget_caption/create_vocab_main.py
+++ widget_caption/create_vocab_main.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Create word vocab for widget captioning model."""
 
 from absl import app
--- widget_caption/create_word_embedding_main.py
+++ widget_caption/create_word_embedding_main.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Generate word embeddings using glove model."""
 from absl import app
 from absl import flags
