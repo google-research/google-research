c375ca927eb8e270b564260fe5fb46ee162a5b8c
Yilei Yang <yileiyang@google.com>
2022-04-14 10:10:29 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 441791526

--- vatt/modeling/heads/bridge.py
+++ vatt/modeling/heads/bridge.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Bridge Contrastive Learning Heads."""
 
 import tensorflow as tf
