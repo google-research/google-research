4d1e0466a59ae85bc615b44312c2172129237b7d
Benjamin Eysenbach <eysenbach@google.com>
2022-06-15 19:34:51 -0700

    Code release for the paper "Contrastive Learning as Goal-Conditioned Reinforcement Learning."
    PiperOrigin-RevId: 455276282

---
+++ contrastive_rl/contrastive/networks.py
@@ -0,0 +1,162 @@
+# coding=utf-8
+# Copyright 2022 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Contrastive RL networks definition."""
+import dataclasses
+from typing import Optional, Tuple, Callable
+
+from acme import specs
+from acme.agents.jax import actor_core as actor_core_lib
+from acme.jax import networks as networks_lib
+from acme.jax import utils
+import haiku as hk
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+
+@dataclasses.dataclass
+class ContrastiveNetworks:
+  """Network and pure functions for the Contrastive RL agent."""
+  policy_network: networks_lib.FeedForwardNetwork
+  q_network: networks_lib.FeedForwardNetwork
+  log_prob: networks_lib.LogProbFn
+  repr_fn: Callable[Ellipsis, networks_lib.NetworkOutput]
+  sample: networks_lib.SampleFn
+  sample_eval: Optional[networks_lib.SampleFn] = None
+
+
+def apply_policy_and_sample(
+    networks,
+    eval_mode = False):
+  """Returns a function that computes actions."""
+  sample_fn = networks.sample if not eval_mode else networks.sample_eval
+  if not sample_fn:
+    raise ValueError('sample function is not provided')
+
+  def apply_and_sample(params, key, obs):
+    return sample_fn(networks.policy_network.apply(params, obs), key)
+  return apply_and_sample
+
+
+def make_networks(
+    spec,
+    obs_dim,
+    repr_dim = 64,
+    repr_norm = False,
+    repr_norm_temp = True,
+    hidden_layer_sizes = (256, 256),
+    actor_min_std = 1e-6,
+    twin_q = False,
+    use_image_obs = False):
+  """Creates networks used by the agent."""
+
+  num_dimensions = np.prod(spec.actions.shape, dtype=int)
+  TORSO = networks_lib.AtariTorso  # pylint: disable=invalid-name
+
+  def _unflatten_obs(obs):
+    state = jnp.reshape(obs[:, :obs_dim], (-1, 64, 64, 3)) / 255.0
+    goal = jnp.reshape(obs[:, obs_dim:], (-1, 64, 64, 3)) / 255.0
+    return state, goal
+
+  def _repr_fn(obs, action, hidden=None):
+    # The optional input hidden is the image representations. We include this
+    # as an input for the second Q value when twin_q = True, so that the two Q
+    # values use the same underlying image representation.
+    if hidden is None:
+      if use_image_obs:
+        state, goal = _unflatten_obs(obs)
+        img_encoder = TORSO()
+        state = img_encoder(state)
+        goal = img_encoder(goal)
+      else:
+        state = obs[:, :obs_dim]
+        goal = obs[:, obs_dim:]
+    else:
+      state, goal = hidden
+
+    sa_encoder = hk.nets.MLP(
+        list(hidden_layer_sizes) + [repr_dim],
+        w_init=hk.initializers.VarianceScaling(1.0, 'fan_avg', 'uniform'),
+        activation=jax.nn.relu,
+        name='sa_encoder')
+    sa_repr = sa_encoder(jnp.concatenate([state, action], axis=-1))
+
+    g_encoder = hk.nets.MLP(
+        list(hidden_layer_sizes) + [repr_dim],
+        w_init=hk.initializers.VarianceScaling(1.0, 'fan_avg', 'uniform'),
+        activation=jax.nn.relu,
+        name='g_encoder')
+    g_repr = g_encoder(goal)
+
+    if repr_norm:
+      sa_repr = sa_repr / jnp.linalg.norm(sa_repr, axis=1, keepdims=True)
+      g_repr = g_repr / jnp.linalg.norm(g_repr, axis=1, keepdims=True)
+
+      if repr_norm_temp:
+        log_scale = hk.get_parameter('repr_log_scale', [], dtype=sa_repr.dtype,
+                                     init=jnp.zeros)
+        sa_repr = sa_repr / jnp.exp(log_scale)
+    return sa_repr, g_repr, (state, goal)
+
+  def _combine_repr(sa_repr, g_repr):
+    return jax.numpy.einsum('ik,jk->ij', sa_repr, g_repr)
+
+  def _critic_fn(obs, action):
+    sa_repr, g_repr, hidden = _repr_fn(obs, action)
+    outer = _combine_repr(sa_repr, g_repr)
+    if twin_q:
+      sa_repr2, g_repr2, _ = _repr_fn(obs, action, hidden=hidden)
+      outer2 = _combine_repr(sa_repr2, g_repr2)
+      # outer.shape = [batch_size, batch_size, 2]
+      outer = jnp.stack([outer, outer2], axis=-1)
+    return outer
+
+  def _actor_fn(obs):
+    if use_image_obs:
+      state, goal = _unflatten_obs(obs)
+      obs = jnp.concatenate([state, goal], axis=-1)
+      obs = TORSO()(obs)
+    network = hk.Sequential([
+        hk.nets.MLP(
+            list(hidden_layer_sizes),
+            w_init=hk.initializers.VarianceScaling(1.0, 'fan_in', 'uniform'),
+            activation=jax.nn.relu,
+            activate_final=True),
+        networks_lib.NormalTanhDistribution(num_dimensions,
+                                            min_scale=actor_min_std),
+    ])
+    return network(obs)
+
+  policy = hk.without_apply_rng(hk.transform(_actor_fn))
+  critic = hk.without_apply_rng(hk.transform(_critic_fn))
+  repr_fn = hk.without_apply_rng(hk.transform(_repr_fn))
+
+  # Create dummy observations and actions to create network parameters.
+  dummy_action = utils.zeros_like(spec.actions)
+  dummy_obs = utils.zeros_like(spec.observations)
+  dummy_action = utils.add_batch_dim(dummy_action)
+  dummy_obs = utils.add_batch_dim(dummy_obs)
+
+  return ContrastiveNetworks(
+      policy_network=networks_lib.FeedForwardNetwork(
+          lambda key: policy.init(key, dummy_obs), policy.apply),
+      q_network=networks_lib.FeedForwardNetwork(
+          lambda key: critic.init(key, dummy_obs, dummy_action), critic.apply),
+      repr_fn=repr_fn.apply,
+      log_prob=lambda params, actions: params.log_prob(actions),
+      sample=lambda params, key: params.sample(seed=key),
+      sample_eval=lambda params, key: params.mode(),
+      )
