d08baa7f6b2fb608a3c121535e372dceb20ae5c7
Kensen Shi <kshi@google.com>
2022-09-21 15:52:00 -0700

    Open-source BUSTLE.
    PiperOrigin-RevId: 475944698

---
+++ bustle/com/googleresearch/bustle/exception/ExecutionException.java
@@ -0,0 +1,8 @@
+package com.googleresearch.bustle.exception;
+
+/**
+ * Represents some issue during execution of a candidate program.
+ */
+public final class ExecutionException extends RuntimeException {
+  public ExecutionException() {}
+}
