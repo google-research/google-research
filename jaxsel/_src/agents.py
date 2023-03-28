a33e7e2064c9ff6cb518541511ffb6aadf14c6b7
Peter Hawkins <phawkins@google.com>
2023-03-28 13:21:12 -0700

    Fix pytype failures related to teaching pytype about NumPy scalar types.
    PiperOrigin-RevId: 520116108

--- jaxsel/_src/agents.py
+++ jaxsel/_src/agents.py
@@ -183,7 +183,7 @@ class Agent(abc.ABC):
 
 
 def product(iterable, start=1.):
-  return functools.reduce(jnp.multiply, iterable, start)
+  return functools.reduce(jnp.multiply, iterable, start)  # pytype: disable=wrong-arg-types  # numpy-scalars
 
 
 @struct.dataclass
