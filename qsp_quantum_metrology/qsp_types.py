d2dfa1ddce460d0d749752324e187467774b1670
Raj Sinha <sinharaj@google.com>
2023-06-04 10:08:48 -0700

    This is the support code for the Paper: Beyond Heisenberg Limit Quantum Metrology through Quantum Signal Processing, https://arxiv.org/abs/2209.11207
    PiperOrigin-RevId: 537697061

---
+++ qsp_quantum_metrology/qsp_types.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+# Copyright 2023 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Types used in this project."""
+
+
+from typing import Annotated, Any, Literal, TypeVar
+
+import numpy as np
+import numpy.typing as npt
+
+
+DType = TypeVar("DType", bound=np.generic)
+
+ArrayAny = Annotated[npt.NDArray[DType], Literal[Any]]
+ArrayAnyAny = Annotated[npt.NDArray[DType], Literal[Any, Any]]
