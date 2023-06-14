1023e235c222760596c4ca911366236ed35e73cc
Gustl22 <git@reb0.org>
2023-06-14 13:27:39 +0200

    Make module

---
+++ screen2words/setup.py
@@ -0,0 +1,23 @@
+"""setup.py for screen2words."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="screen2words",
+    version="0.1",
+    packages=find_packages(),
+    author="Bryan Wang",
+    author_email="bryanw@dgp.toronto.edu",
+    install_requires=[
+       # "scikit-learn==0.19.2",
+       # "tensorflow-datasets==2.1.0",
+       # "tensorflow-gpu==2.1.0",
+       # "matplotlib==2.2.4",
+       # "scipy==1.2.2",
+       # "tf-models-official"
+       # "PyYaml==5.3",
+       # "jupyter==1.0.0",
+       # "dec_da @ https://github.com/dizcology/DEC-DA/archive/056079d05008da27961ab90cb68c66591ba2187f.zip",
+    ],
+)
