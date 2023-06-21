32d7e53a1bfedb36d659bc44cb03d93f2aef2c9b
Si-An Chen <sianchen@google.com>
2023-06-20 23:47:30 -0700

    first commit of TSMixer
    PiperOrigin-RevId: 542161389

---
+++ tsmixer/tsmixer_basic/models/tsmixer_rev_in.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+# Copyright 2023 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Implementation of TSMixer with Reversible Instance Normalization."""
+
+from models.RevIN import RevNorm
+from models.TSMixer import res_block
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+def build_model(
+    input_shape,
+    pred_len,
+    norm_type,
+    activation,
+    n_block,
+    dropout,
+    ff_dim,
+    target_slice,
+):
+  """Build TSMixer with Reversible Instance Normalization model."""
+
+  inputs = tf.keras.Input(shape=input_shape)
+  x = inputs  # [Batch, Input Length, Channel]
+  rev_norm = RevNorm(axis=-2)
+  x = rev_norm(x, 'norm')
+  for _ in range(n_block):
+    x = res_block(x, norm_type, activation, dropout, ff_dim)
+
+  if target_slice:
+    x = x[:, :, target_slice]
+
+  x = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Channel, Input Length]
+  x = layers.Dense(pred_len)(x)  # [Batch, Channel, Output Length]
+  outputs = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Output Length, Channel])
+  outputs = rev_norm(outputs, 'denorm', target_slice)
+  return tf.keras.Model(inputs, outputs)
