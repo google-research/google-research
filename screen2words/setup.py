5c1f396a26e9f05a06efbfc65d6d07da336a0758
Gustl22 <git@reb0.org>
2023-11-23 15:29:40 +0100

    Setup.py

--- screen2words/setup.py
+++ screen2words/setup.py
@@ -15,14 +15,19 @@ setup(
     author_email="bryanw@dgp.toronto.edu",
     install_requires=[
         "absl"
+        "numpy"
         "apache_beam>=2.52.0"
         "rouge-score>=0.1.2",
         "nltk>=3.8.1",
         "keras>=2.12.0",
+        "protobuf<4.20.0",
+        # On Windows only
+        # "cython<3.0.0",
+        # "pyyaml==5.4.1", # pip install pyyaml==5.4.1 --no-build-isolation
         "tensorflow>=2.12.0",
         "tensorflow-intel>=2.12.0",
         "tf-models-official>=2.12.0",
-        "tensorflow-text>=2.12.1",
+        "tensorflow-text>=2.12.1", # Need to compile from source on Windows, see https://github.com/tensorflow/text#a-note-about-different-operating-system-packages
         "tensorflow-metadata>=1.13.1"
     ],
 )
