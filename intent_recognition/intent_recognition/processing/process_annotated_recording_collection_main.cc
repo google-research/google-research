d681124a75d1ee55392b9823412a5f8201494fbd
Google Research Team <no-reply@google.com>
2024-01-15 02:51:42 -0800

    Internal change
    PiperOrigin-RevId: 598558257

--- intent_recognition/intent_recognition/processing/process_annotated_recording_collection_main.cc
+++ intent_recognition/intent_recognition/processing/process_annotated_recording_collection_main.cc
@@ -109,7 +109,7 @@ absl::Status ProcessAnnotatedRecordingCollection(
   // Create the graph config based on the processing options.
   mediapipe::CalculatorGraphConfig graph_config =
       BuildDrishtiGraphWithProcessingOptions(proto_string, processing_options);
-  LOG(INFO) << "Graph config: " << graph_config.DebugString();
+  LOG(INFO) << "Graph config: " << graph_config;
 
   // Read input records, process them, and store the result.
   AnnotatedRecordingCollection input_arc;
