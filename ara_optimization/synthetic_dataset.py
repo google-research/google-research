32695564d3047db9fb00a448e329de2b74b8e491
Google Research Team <no-reply@google.com>
2024-03-03 22:43:32 -0800

    Fix or ignore some pytype errors.
    PiperOrigin-RevId: 612321881

--- ara_optimization/synthetic_dataset.py
+++ ara_optimization/synthetic_dataset.py
@@ -339,7 +339,7 @@ def generate_slice_distribution_with_conversions_raw(
                 1,
                 conv_value[conv_id],
             ])
-  return dataset
+  return dataset  # pytype: disable=bad-return-type
 
 
 def generate_counts_and_values_dataset_raw(
