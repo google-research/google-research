e14a9134ddcbe49a6c3fefedc25386306a3324cc
Shuyang Sun <kevinssy@qq.com>
2024-03-12 22:51:47 -0700

    Merge of PR #1953
    PiperOrigin-RevId: 615292969

---
+++ clip_as_rnn/modeling/model/model.py
@@ -0,0 +1,322 @@
+# coding=utf-8
+# Copyright 2024 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Implementation of CamCut."""
+
+import os
+
+import clip
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+# pylint: disable=g-importing-member
+from clip_as_rnn.modeling.model.clip_wrapper import CLIPWrapper
+from clip_as_rnn.modeling.model.clip_wrapper import forward_clip
+from clip_as_rnn.modeling.model.clipcam import CLIPCAM
+from clip_as_rnn.modeling.model.crf import PostProcess
+from clip_as_rnn.modeling.model.utils import apply_visual_prompts
+from clip_as_rnn.modeling.model.utils.visualize import viz_attn
+
+
+class CamCut(nn.Module):
+  """CamCat module."""
+
+  def __init__(
+      self,
+      cfg,
+      device="cpu",
+      visualize=False,
+      confidence_threshold=0.45,
+      save_path="save_path",
+      seg_mode="refer",
+      semantic_clip_model_name=None,
+      semantic_pretrained_data=None,
+      semantic_templates=None,
+      text_template=None,
+      visual_prompt_type="circle",
+      clipes_threshold=0.4,
+      cam_text_template="a clean origami {}.",
+      bg_cls=None,
+      iom_thres=0.6,
+      min_pred_threshold=0.01,
+      bg_factor=1.0,
+      mask_threshold=0.5,
+  ):
+    """CamCut model for image segmentation.
+
+    Args:
+        cfg: the config file.
+        device: the device to run the model.
+        visualize: whether to visualize the intermediate results
+        confidence_threshold: the confidence threshold for semantic
+          segmentation. If the confidence score is lower than the threshold, the
+          mask will be discarded.
+        save_path: the path to save the intermediate results
+        seg_mode: the segmentation mode, can be 'refer' or 'semantic'
+        semantic_clip_model_name: the name of the semantic segmentation model.
+        semantic_pretrained_data: the path to the pretrained semantic
+          segmentation model.
+        semantic_templates: the templates for semantic segmentation.
+        text_template: the template for visual prompting.
+        visual_prompt_type: the type of visual prompting.
+        clipes_threshold: the threshold for CLIPES.
+        cam_text_template: the template for CAM.
+        bg_cls: background classes.
+        iom_thres: IoM threshold.
+        min_pred_threshold: Prediction threshold.
+        bg_factor: Background factor.
+        mask_threshold: Mask threshold.
+    """
+    super(CamCut, self).__init__()
+    # CLIP parameters
+    self.confidence_threshold = confidence_threshold
+    self.device = device
+    self.visualize = visualize
+    self.save_path = save_path
+    self.seg_mode = seg_mode
+    self.semantic_clip_model_name = semantic_clip_model_name
+    self.semantic_pretrained_data = semantic_pretrained_data
+    self.semantic_templates = semantic_templates
+    self.text_template = text_template
+    self.visual_prompt_type = visual_prompt_type
+    self.clipes_threshold = clipes_threshold
+    self.cam_text_template = cam_text_template
+    self.iom_thres = iom_thres
+    self.min_pred_threshold = min_pred_threshold
+    self.bg_cls = bg_cls
+    self.bg_factor = bg_factor
+    self.mask_threshold = mask_threshold
+
+    if not hasattr(cfg, "clip"):
+      raise ValueError("The config file should contain the CLIP parameters.")
+
+    if not hasattr(cfg, "camcut"):
+      raise ValueError("The config file should contain the camcut parameters.")
+
+    if hasattr(cfg, "cam"):
+      raise ValueError("cfg.cam is deprecated, please use cfg.camcut ")
+
+    for k, v in vars(cfg.clip).items():
+      setattr(self, k, v)
+
+    for k, v in vars(cfg.camcut).items():
+      setattr(self, k, v)
+
+    if hasattr(cfg, "sam"):
+      for k, v in vars(cfg.sam).items():
+        setattr(self, k, v)
+    if not self.bg_cls:
+      self.bg_cls = None
+    print(f"The model is running on {self.device}")
+    self.clip_model, self.preprocess = clip.load(
+        self.clip_model_name, device=self.device
+    )
+    self.clip_model = CLIPWrapper(self.clip_model)
+    self.post_process = PostProcess(device=self.device)
+    self.mask_generator = CLIPCAM(
+        self.clip_model,
+        device=self.device,
+        text_template=self.text_template,
+        threshold=self.clipes_threshold,
+        bg_cls=self.bg_cls,
+    )
+    self.semantic_clip_model, self.semantic_preprocess = clip.load(
+        self.semantic_clip_model_name, device=self.device
+    )
+    self.semantic_clip_model = CLIPWrapper(self.semantic_clip_model)
+
+  def get_confidence(self, cam_map, binary_cam_map):
+    confidence_map = torch.sum(cam_map * binary_cam_map[None], dim=[2, 3])
+    confidence_map = confidence_map / torch.sum(binary_cam_map, dim=[1, 2])
+    confidence_score = confidence_map.squeeze()
+    return confidence_score
+
+  def set_visual_prompt_type(self, visual_prompt_type):
+    self.visual_prompt_type = visual_prompt_type
+
+  def set_bg_factor(self, bg_factor):
+    self.bg_factor = bg_factor
+
+  def set_confidence_threshold(self, confidence_threshold):
+    self.confidence_threshold = confidence_threshold
+
+  def set_mask_threshold(self, mask_threshold):
+    self.mask_threshold = mask_threshold
+
+  def apply_visual_prompts(self, image, mask):
+    if torch.sum(mask).item() <= 1:
+      return image
+    image_array = np.array(image)
+    img_h = image_array.shape[0]
+    img_w = image_array.shape[1]
+    mask = (
+        F.interpolate(mask[None][None], size=(img_h, img_w), mode="nearest")
+        .squeeze()
+        .detach()
+        .cpu()
+        .numpy()
+    )
+    mask = (mask > self.mask_threshold).astype(np.uint8)
+    prompted_image = apply_visual_prompts(
+        image_array, mask, self.visual_prompt_type, self.visualize
+    )
+    return prompted_image
+
+  def get_mask_confidence(self, prompted_images, prompt_text):
+    """Get the confidene for each mask with visual prompting."""
+    # get the center, width and height of the mask
+    prompted_tensor = torch.stack(
+        [self.semantic_preprocess(img) for img in prompted_images], dim=0
+    )
+    prompted_tensor = prompted_tensor.to(self.device)
+    h, w = prompted_tensor.shape[-2:]
+    text_prediction = forward_clip(
+        self.semantic_clip_model, prompted_tensor, prompt_text, h, w
+    )
+    return text_prediction
+
+  def _filter_masks(self, ori_mask_id, sem_scores, prompt_text):
+    """Remove false positive masks by score filtering and recall the backbone to get the CAM maps for the filtered texts."""
+    if not ori_mask_id:
+      max_id = np.argmax(sem_scores)
+      ori_mask_id.append(max_id)
+    filtered_text = [prompt_text[i] for i in ori_mask_id]
+    return filtered_text
+
+  def _forward_stage(self, ori_img, cam_text, clip_text, semantic_prompt_text):
+    mask_proposals = self.get_cam_map(ori_img, cam_text)
+    num_texts = len(clip_text)
+    ori_mask_id = []
+    sem_scores = torch.zeros((num_texts,), device=self.device).float()
+    prompted_imgs = [
+        self.apply_visual_prompts(ori_img, cam_map)
+        for cam_map in mask_proposals
+    ]
+    text_scores = self.get_mask_confidence(prompted_imgs, semantic_prompt_text)
+    mask_scores = torch.diagonal(text_scores)
+    for mask_idx, mask_score in enumerate(mask_scores):
+      # record mask idx
+      if mask_score > self.confidence_threshold:
+        ori_mask_id.append(mask_idx)
+      sem_scores[mask_idx] = mask_score
+    sem_scores = sem_scores.cpu().detach().numpy()
+    filtered_texts = self._filter_masks(ori_mask_id, sem_scores, clip_text)
+    if isinstance(ori_img, list):
+      ori_img = [ori_img[i] for i in ori_mask_id]
+
+    # map the refined mask to the original mask
+    pseudo_masks = torch.zeros_like(mask_proposals)
+    all_scores = torch.zeros((num_texts,), device=self.device).float()
+    sem_scores = torch.from_numpy(sem_scores).to(self.device)
+    for new_id, ori_id in enumerate(ori_mask_id):
+      if new_id >= len(mask_proposals):
+        # the mask is filtered out.
+        continue
+      pseudo_masks[ori_id] = mask_proposals[new_id]
+      all_scores[ori_id] = sem_scores[ori_id]
+    return mask_proposals, filtered_texts, ori_img, all_scores
+
+  def _get_save_path(self, text):
+    folder_name = "_".join([t.replace(" ", "_") for t in text])
+    if len(folder_name) > 20:
+      folder_name = folder_name[:20]
+    output_path = os.path.join(self.save_path, folder_name)
+    sub_output_path = [
+        os.path.join(output_path, t.replace(" ", "_")) for t in text
+    ]
+    return output_path, sub_output_path
+
+  def get_cam_map(self, img, text):
+    if self.seg_mode == "refer":
+      if isinstance(img, list):
+        cam_map_list = [self.mask_generator(i, t)[0] for i, t in zip(img, text)]
+      else:
+        cam_map_list = [self.mask_generator(img, t)[0] for t in text]
+      return torch.cat(cam_map_list, dim=0)
+    elif self.seg_mode == "semantic":
+      return self.mask_generator(img, text)[0]
+    else:
+      raise ValueError(
+          "Unknown segmentation mode. Only refer and semantic are supported."
+      )
+
+  def _forward_camcut(self, ori_img, text):
+    if isinstance(text, str):
+      text = [text]
+    _, sub_output_path = self._get_save_path(text)
+    image_array = np.array(ori_img)
+    clip_text = [self.cam_text_template.format(t) for t in text]
+    cam_text = text
+    cam_map_list = self.get_cam_map(ori_img, text)
+    if self.visualize:
+      _ = [
+          viz_attn(
+              image_array,
+              attn,
+              prefix=sub_output_path[aid],
+              img_name="cam_map",
+          )
+          for aid, attn in enumerate(cam_map_list)
+      ]
+    semantic_prompt_text = clip_text
+    if self.semantic_templates is not None:
+      semantic_prompt_text = []
+      for template in self.semantic_templates:
+        templated_text = [template.format(t) for t in text]
+        semantic_prompt_text.append(templated_text)
+
+    num_positive_last = 0
+    last_mask_proposals = []
+    while True:
+      cam_map_list, all_texts, ori_img, all_scores = self._forward_stage(
+          ori_img, cam_text, clip_text, semantic_prompt_text
+      )
+      if all_texts:  # if there is no text, skip the refinement
+        cam_text = all_texts
+      if (cam_map_list.max() == 0).item():
+        cam_map_list = last_mask_proposals
+
+      num_positive = (all_scores > 0).sum().item()
+      # cam_map_list = pseudo_masks
+      if num_positive <= 1 or num_positive == num_positive_last:
+        # stop the refinement if there is only one mask or the number
+        # of positive masks does not change.
+        break
+      num_positive_last = num_positive
+    # apply densecrf for refinement
+    pseudo_masks = self.post_process(
+        ori_img,
+        cam_map_list,
+        separate=self.seg_mode == "refer",
+        bg_factor=self.bg_factor,
+    )
+    if self.visualize:
+      _ = [
+          viz_attn(
+              image_array,
+              attn,
+              prefix=sub_output_path[aid],
+              img_name="semantic_mask",
+          )
+          for aid, attn in enumerate(pseudo_masks)
+      ]
+    return pseudo_masks, all_scores, cam_map_list
+
+  def forward(self, im_ori, text):
+    # raw_image_np is the padded image input with shape (512, 512, 3)
+    pseudo_masks, conf_scores, cam_map = self._forward_camcut(im_ori, text)
+    return pseudo_masks, conf_scores, cam_map
---
+++ clip_as_rnn/modeling/post_process/object_discovery.py
@@ -0,0 +1,355 @@
+# coding=utf-8
+# Copyright 2024 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Find objects."""
+
+# pylint: disable=g-importing-member
+import numpy as np
+import scipy
+from scipy import ndimage
+from scipy.linalg import eigh
+from scipy.ndimage import label
+import torch
+import torch.nn.functional as F
+
+
+def ncut(
+    feats,
+    dims,
+    scales,
+    init_image_size,
+    tau=0,
+    eps=1e-5,
+    no_binary_graph=False,
+):
+  """Implementation of NCut Method.
+
+  Args:
+    feats: the pixel/patche features of an image
+    dims: dimension of the map from which the features are used
+    scales: from image to map scale
+    init_image_size: size of the image
+    tau: thresold for graph construction
+    eps: graph edge weight
+    no_binary_graph: ablation study for using similarity score as graph
+      edge weight
+  Returns:
+    TODO
+  """
+  feats = feats[0, 1:, :]
+  feats = F.normalize(feats, p=2)
+  a = feats @ feats.transpose(1, 0)
+  a = a.cpu().numpy()
+  if no_binary_graph:
+    a[a < tau] = eps
+  else:
+    a = a > tau
+    a = np.where(a.astype(float) == 0, eps, a)
+  d_i = np.sum(a, axis=1)
+  d = np.diag(d_i)
+
+  # Print second and third smallest eigenvector
+  _, eigenvectors = eigh(d - a, d, subset_by_index=[1, 2])
+  eigenvec = np.copy(eigenvectors[:, 0])
+
+  # Using average point to compute bipartition
+  second_smallest_vec = eigenvectors[:, 0]
+  avg = np.sum(second_smallest_vec) / len(second_smallest_vec)
+  bipartition = second_smallest_vec > avg
+
+  seed = np.argmax(np.abs(second_smallest_vec))
+
+  if bipartition[seed] != 1:
+    eigenvec = eigenvec * -1
+    bipartition = np.logical_not(bipartition)
+  bipartition = bipartition.reshape(dims).astype(float)
+
+  # predict BBox
+  # We only extract the principal object BBox
+  pred, _, objects, cc = detect_box(
+      bipartition,
+      seed,
+      dims,
+      scales=scales,
+      initial_im_size=init_image_size[1:],
+  )
+  mask = np.zeros(dims)
+  mask[cc[0], cc[1]] = 1
+
+  return np.asarray(pred), objects, mask, seed, None, eigenvec.reshape(dims)
+
+
+def grad_obj_discover_on_attn(attn, gradcam, dims, topk=1, threshold=0.6):
+  """Get the gradcam and attn map, then find the seed, then use LOST algorithm to find the potential points.
+
+  Args:
+      attn: attention map from ViT averaged across all heads, shape: [1,
+        (1+num_patches), (1+num_patches)].
+      gradcam: gradcam map from ViT, shape: [1, 1, H, W].
+      dims:
+      topk:
+      threshold:
+  Returns:
+      th_attn:
+  """
+
+  w_featmap, h_featmap = dims
+  # nh = attn.shape[1]
+  attn = attn.squeeze()
+
+  seeds = torch.argsort(gradcam.flatten(), descending=True)[:topk]
+
+  # We keep only the output patch attention
+  # Get the attentions corresponding to [CLS] token
+  patch_attn = attn[1:, 1:]
+  topk_attn = patch_attn[seeds]
+  nh = topk_attn.shape[0]
+  # attentions = attn[0, :, 0, 1:].reshape(nh, -1)
+
+  # we keep only a certain percentage of the mass
+  val, idx = torch.sort(topk_attn)
+  val /= torch.sum(val, dim=1, keepdim=True)
+  cumval = torch.cumsum(val, dim=1)
+  th_attn = cumval > (1 - threshold)
+  idx2 = torch.argsort(idx)
+  for h in range(nh):
+    th_attn[h] = th_attn[h][idx2[h]]
+  th_attn = th_attn.reshape(nh, w_featmap, h_featmap).float()
+  th_attn = th_attn.sum(0)
+  th_attn[th_attn > 1] = 1
+  return th_attn[None, None]
+
+
+def grad_obj_discover(feats, gradcam, dims):
+  """Using gradient heatmap to find the seed, then use LOST algorithm to find the potential points.
+
+  Args:
+      feats: the pixel/patche features of an image. Shape: [1, HW, C]
+      gradcam: the grad cam map
+      dims: dimension of the map from which the features are used
+
+  Returns:
+      pred: box predictions
+      A: binary affinity matrix
+      scores: lowest degree scores for all patches
+      seed: selected patch corresponding to an object
+  """
+  # Compute the similarity
+  a = (feats @ feats.transpose(1, 2)).squeeze()
+
+  # Compute the inverse degree centrality measure per patch
+  # sorted_patches, scores = patch_scoring(a)
+
+  # Select the initial seed
+  # seed = sorted_patches[0]
+  seed = gradcam.argmax()
+  mask = a[seed]
+  mask = mask.view(1, 1, *dims)
+
+  return mask
+
+
+def lost(feats, dims, scales, init_image_size, k_patches=100):
+  """Implementation of LOST method.
+
+  Args:
+      feats: the pixel/patche features of an image. Shape: [1, C, H, W]
+      dims: dimension of the map from which the features are used
+      scales: from image to map scale
+      init_image_size: size of the image
+      k_patches: number of k patches retrieved that are compared to the seed
+          at seed expansion.
+  Returns:
+      pred: box predictions
+      A: binary affinity matrix
+      scores: lowest degree scores for all patches
+      seed: selected patch corresponding to an object
+  """
+  # Compute the similarity
+  feats = feats.flatten(2).transpose(1, 2)
+  a = (feats @ feats.transpose(1, 2)).squeeze()
+
+  # Compute the inverse degree centrality measure per patch
+  sorted_patches, _ = patch_scoring(a)
+
+  # Select the initial seed
+  seed = sorted_patches[0]
+
+  # Seed expansion
+  potentials = sorted_patches[:k_patches]
+  similars = potentials[a[seed, potentials] > 0.0]
+  m = torch.sum(a[similars, :], dim=0)
+
+  # Box extraction
+  _, _, _, mask = detect_box(
+      m, seed, dims, scales=scales, initial_im_size=init_image_size[1:]
+  )
+
+  return mask
+  # return np.asarray(bbox), A, scores, seed
+
+
+def patch_scoring(m, threshold=0.0):
+  """Patch scoring based on the inverse degree."""
+  # Cloning important
+  a = m.clone()
+
+  # Zero diagonal
+  a.fill_diagonal_(0)
+
+  # Make sure symmetric and non nul
+  a[a < 0] = 0
+  # C = A + A.t()
+
+  # Sort pixels by inverse degree
+  cent = -torch.sum(a > threshold, dim=1).type(torch.float32)
+  sel = torch.argsort(cent, descending=True)
+
+  return sel, cent
+
+
+def detect_box(
+    bipartition,
+    seed,
+    dims,
+    initial_im_size=None,
+    scales=None,
+    principle_object=True,
+):
+  """Extract a box corresponding to the seed patch."""
+
+  # Among connected components extract from the affinity matrix, select the one
+  # corresponding to the seed patch.
+
+  # w_featmap, h_featmap = dims
+  objects, _ = ndimage.label(bipartition)
+  cc = objects[np.unravel_index(seed, dims)]
+
+  if principle_object:
+    mask = np.where(objects == cc)
+    # Add +1 because excluded max
+    ymin, ymax = min(mask[0]), max(mask[0]) + 1
+    xmin, xmax = min(mask[1]), max(mask[1]) + 1
+    # Rescale to image size
+    r_xmin, r_xmax = scales[1] * xmin, scales[1] * xmax
+    r_ymin, r_ymax = scales[0] * ymin, scales[0] * ymax
+    pred = [r_xmin, r_ymin, r_xmax, r_ymax]
+
+    # Check not out of image size (used when padding)
+    if initial_im_size:
+      pred[2] = min(pred[2], initial_im_size[1])
+      pred[3] = min(pred[3], initial_im_size[0])
+
+    # Coordinate predictions for the feature space
+    # Axis different then in image space
+    pred_feats = [ymin, xmin, ymax, xmax]
+
+    return pred, pred_feats, objects, mask
+  else:
+    raise NotImplementedError
+
+
+# This function is modified from
+# https://github.com/facebookresearch/dino/blob/main/visualize_attention.py
+# Ref: https://github.com/facebookresearch/dino.
+def dino_seg(attn, dims, patch_size, head=0):
+  """Extraction of boxes based on the DINO segmentation method proposed in DINO."""
+  w_featmap, h_featmap = dims
+  nh = attn.shape[1]
+  official_th = 0.6
+
+  # We keep only the output patch attention
+  # Get the attentions corresponding to [CLS] token
+  attentions = attn[0, :, 0, 1:].reshape(nh, -1)
+
+  # we keep only a certain percentage of the mass
+  val, idx = torch.sort(attentions)
+  val /= torch.sum(val, dim=1, keepdim=True)
+  cumval = torch.cumsum(val, dim=1)
+  th_attn = cumval > (1 - official_th)
+  idx2 = torch.argsort(idx)
+  for h in range(nh):
+    th_attn[h] = th_attn[h][idx2[h]]
+  th_attn = th_attn.reshape(nh, w_featmap, h_featmap).float()
+
+  # Connected components
+  labeled_array, _ = scipy.ndimage.label(th_attn[head].cpu().numpy())
+
+  # Find the biggest component
+  size_components = [
+      np.sum(labeled_array == c) for c in range(np.max(labeled_array))
+  ]
+
+  if len(size_components) > 1:
+    # Select the biggest component avoiding component 0 corresponding
+    # to background
+    biggest_component = np.argmax(size_components[1:]) + 1
+  else:
+    # Cases of a single component
+    biggest_component = 0
+
+  # Mask corresponding to connected component
+  mask = np.where(labeled_array == biggest_component)
+
+  # Add +1 because excluded max
+  ymin, ymax = min(mask[0]), max(mask[0]) + 1
+  xmin, xmax = min(mask[1]), max(mask[1]) + 1
+
+  # Rescale to image
+  r_xmin, r_xmax = xmin * patch_size, xmax * patch_size
+  r_ymin, r_ymax = ymin * patch_size, ymax * patch_size
+  pred = [r_xmin, r_ymin, r_xmax, r_ymax]
+
+  return pred
+
+
+def get_feats(feat_out, shape):
+  # Batch size, Number of heads, Number of tokens
+  nb_im, nh, nb_tokens = shape[0:3]
+  qkv = (
+      feat_out["qkv"]
+      .reshape(nb_im, nb_tokens, 3, nh, -1 // nh)
+      .permute(2, 0, 3, 1, 4)
+  )
+  k = qkv[1]
+  k = k.transpose(1, 2).reshape(nb_im, nb_tokens, -1)
+  return k
+
+
+def get_instances(masks, return_largest=False):
+  return [
+      get_instances_single(m[None], return_largest=return_largest)
+      for m in masks
+  ]
+
+
+def get_instances_single(mask, return_largest=False):
+  """Get the mask of a single instance."""
+  labeled_array, _ = label(mask.cpu().numpy())
+  instances = np.concatenate(
+      [labeled_array == c for c in range(np.max(labeled_array) + 1)], axis=0
+  )
+  if return_largest:
+    size_components = np.sum(instances, axis=(1, 2))
+    if len(size_components) > 1:
+      # Select the biggest component avoiding component 0 corresponding
+      # to background
+      biggest_component = np.argmax(size_components[1:]) + 1
+    else:
+      # Cases of a single component
+      biggest_component = 0
+    # Mask corresponding to connected component
+    return torch.from_numpy(labeled_array == biggest_component).float()
+  return torch.from_numpy(instances[1:]).float()
