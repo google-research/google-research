51a31aeb5511795c404b0ade9f203f013f8434f7
Emir Ulurak <emir.ulurak@ozu.edu.tr>
2024-04-26 17:01:40 +0300

    .

--- dpok/rarity_score_run.py
+++ dpok/rarity_score_run.py
@@ -2,6 +2,15 @@ import argparse
 from dpok_nft.ImageReward.models.RarityScore import RarityScore
 import os
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--vit_weights", required=True, help="Path to the pretrained ViT model weights file.")
+    parser.add_argument("--generated_images_path", nargs="+", required=True, help="Paths to the generated images.")
+    parser.add_argument("--enable_weighting", action="store_true", help="Enable weighted averaging of scores.")
+    parser.add_argument("--enable_mean", action="store_true", help="Enable mean calculation of scores.")
+    return parser.parse_args()
+    
+
 def main(vit_weights_path, generated_images_path):
     if args.enable_weighting:
         rare_weight = 1.2
@@ -32,15 +41,8 @@ def main(vit_weights_path, generated_images_path):
 
 
 if __name__ == "__main__":
-    #parser = argparse.ArgumentParser(description="Compute RarityScore for generated images.")
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--vit_weights", required=True, help="Path to the pretrained ViT model weights file.")
-    parser.add_argument("--generated_images_path", nargs="+", required=True, help="Paths to the generated images.")
-    parser.add_argument("--enable_weighting", action="store_true", help="Enable weighted averaging of scores.")
-    parser.add_argument("--enable_mean", action="store_true", help="Enable mean calculation of scores.")
-    args = parser.parse_args()
-
-    main(args.vit_weights, args.generated_images_path, args)
+    args = parse_arguments()
+    main(args.vit_weights, args.generated_images_path)
 
 # Example usage:
 # python rarity_score_run.py --vit_weights path/to/pretrained/vit/model/weights --generated_images_path path/to/generated/images/folder
