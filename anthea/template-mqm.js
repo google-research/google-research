5587de72de5370837c4f059c66281a212f7d0f58
Alison Lui <alisonlui@google.com>
2024-05-28 17:27:13 -0700

    Introduce 'Gender Mismatch' and 'Archaic or obscure word choice' error categories.
    * 'Gender Mismatch' is now a separate category under 'Accuracy' (formerly this category was included in 'Accuracy/mistranslation', but should now be marked separately).
    * 'Archaic or obscure word choice' has been added under 'Style'.
    
    PiperOrigin-RevId: 638082297

--- anthea/template-mqm.js
+++ anthea/template-mqm.js
@@ -106,6 +106,10 @@ antheaTemplates['MQM'] = {
           display: 'Mistranslation',
           description: 'The target text does not accurately represent the source text.',
         },
+        gender_mismatch: {
+          display: 'Gender Mismatch',
+          description: 'The gender is incorrect (incorrect pronouns, noun/adjective endings, etc).',
+        },
         untranslated: {
           display: 'Source language fragment',
           description: 'Content that should have been translated has been left untranslated.',
@@ -166,6 +170,10 @@ antheaTemplates['MQM'] = {
         sentence_structure: {
           display: 'Bad sentence structure',
           description: 'The marked span of text is an unnecessary repetition, or makes the sentence unnecessarily long, or would have been better as a clause in the previous sentence.'
+        },
+        archaic_word: {
+          display: 'Archaic or obscure word choice',
+          description: 'An archaic or lesser-known word is used where a more colloquial term would be a better fit.',
         },
       },
     },
