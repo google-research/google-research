6566900a1dfeaa6df472717dec53fc9ddc2528ed
Google Research Team <no-reply@google.com>
2025-04-02 11:52:57 -0700

    Internal change
    PiperOrigin-RevId: 743223944

--- jrl/agents/bc/networks.py
+++ jrl/agents/bc/networks.py
@@ -27,6 +27,7 @@ import haiku as hk
 import jax
 import jax.numpy as jnp
 import numpy as np
+
 from jrl.utils.networks import procgen_networks
 
 distributional = networks_lib.distributional
@@ -36,6 +37,7 @@ atari = networks_lib.atari
 @dataclasses.dataclass
 class BCNetworks:
   """Network and pure functions for the BC agent."""
+
   policy_network: networks_lib.FeedForwardNetwork
   log_prob: networks_lib.LogProbFn
   sample: networks_lib.SampleFn
@@ -44,7 +46,8 @@ class BCNetworks:
 
 
 def apply_policy_and_sample(
-    networks, eval_mode = False):
+    networks, eval_mode = False
+):
   """Returns a function that computes actions."""
   sample_fn = networks.sample if not eval_mode else networks.sample_eval
   if not sample_fn:
@@ -52,11 +55,13 @@ def apply_policy_and_sample(
 
   def apply_and_sample(params, key, obs):
     return sample_fn(networks.policy_network.apply(params, obs), key)
+
   return actor_core.batched_feed_forward_to_actor_core(apply_and_sample)
 
 
 def apply_policy_and_sample_with_img_encoder(
-    networks, eval_mode = False):
+    networks, eval_mode = False
+):
   """Returns a function that computes actions."""
   sample_fn = networks.sample if not eval_mode else networks.sample_eval
   if not sample_fn:
@@ -64,20 +69,28 @@ def apply_policy_and_sample_with_img_encoder(
 
   def apply_and_sample(params, key, obs):
     img = obs['state_image']
+    if networks.img_encoder is None:
+      raise ValueError('networks.img_encoder not initialized.')
     img_embedding = networks.img_encoder.apply(params[1], img)
     x = dict(state_image=img_embedding, state_dense=obs['state_dense'])
     return sample_fn(networks.policy_network.apply(params[0], x), key)
+
   return actor_core.batched_feed_forward_to_actor_core(apply_and_sample)
 
-w_init = hk.initializers.VarianceScaling(1.0, "fan_avg", "truncated_normal")
+
+w_init = hk.initializers.VarianceScaling(1.0, 'fan_avg', 'truncated_normal')
 b_init = jnp.zeros
-dist_w_init = hk.initializers.VarianceScaling(1.0, "fan_avg", "truncated_normal")
+dist_w_init = hk.initializers.VarianceScaling(
+    1.0, 'fan_avg', 'truncated_normal'
+)
 dist_b_init = jnp.zeros
+
 
 @gin.register
 def build_standard_actor_fn(
     num_dimensions,
-    actor_hidden_layer_sizes = (256, 256, 256),):
+    actor_hidden_layer_sizes = (256, 256, 256),
+):
   def _actor_fn(obs):
     # # for matching Ilya's codebase
     # relu_orthogonal = hk.initializers.Orthogonal(scale=2.0**0.5)
@@ -100,7 +113,8 @@ def build_standard_actor_fn(
             w_init=w_init,
             b_init=b_init,
             activation=jax.nn.relu,
-            activate_final=True),
+            activate_final=True,
+        ),
         # networks_lib.NormalTanhDistribution(num_dimensions),
         networks_lib.NormalTanhDistribution(
             num_dimensions,
@@ -110,6 +124,7 @@ def build_standard_actor_fn(
         ),
     ])
     return network(obs)
+
   return _actor_fn
 
 
@@ -117,7 +132,7 @@ def make_networks(
     spec,
     build_actor_fn=build_standard_actor_fn,
     img_encoder_fn=None,
-    ):
+):
   """Creates networks used by the agent."""
   # Create dummy observations and actions to create network parameters.
   dummy_action = utils.zeros_like(spec.actions)
@@ -135,15 +150,20 @@ def make_networks(
 
   if img_encoder_fn is not None:
     img_encoder = hk.without_apply_rng(
-        hk.transform(img_encoder_fn, apply_rng=True))
+        hk.transform(img_encoder_fn, apply_rng=True)
+    )
     key = jax.random.PRNGKey(seed=42)
     temp_encoder_params = img_encoder.init(key, dummy_obs['state_image'])
-    dummy_hidden = img_encoder.apply(temp_encoder_params, dummy_obs['state_image'])
+    dummy_hidden = img_encoder.apply(
+        temp_encoder_params, dummy_obs['state_image']
+    )
     img_encoder_network = networks_lib.FeedForwardNetwork(
-        lambda key: img_encoder.init(key, dummy_hidden), img_encoder.apply)
+        lambda key: img_encoder.init(key, dummy_hidden), img_encoder.apply
+    )
     dummy_policy_input = dict(
         state_image=dummy_hidden,
-        state_dense=dummy_obs['state_dense'],)
+        state_dense=dummy_obs['state_dense'],
+    )
   else:
     img_encoder_fn = None
     dummy_policy_input = dummy_obs
@@ -153,8 +173,10 @@ def make_networks(
 
   return BCNetworks(
       policy_network=networks_lib.FeedForwardNetwork(
-          lambda key: policy.init(key, dummy_policy_input), policy.apply),
+          lambda key: policy.init(key, dummy_policy_input), policy.apply
+      ),
       log_prob=lambda params, actions: params.log_prob(actions),
       sample=lambda params, key: params.sample(seed=key),
       sample_eval=lambda params, key: params.mode(),
-      img_encoder=img_encoder_network,)
+      img_encoder=img_encoder_network,
+  )
