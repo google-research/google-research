4ba477b21ad8385c678f66974ab17a31a1332d35
Oleh Prypin <oprypin@google.com>
2025-06-16 01:34:43 -0700

    Replace obsolete `import contextlib2` with the equivalent stdlib module `contextlib`
    PiperOrigin-RevId: 771916413

--- aptamers_mlpd/learning/dataset_stats.py
+++ aptamers_mlpd/learning/dataset_stats.py
@@ -15,10 +15,10 @@
 
 """Utilities for calculating dataset statistics."""
 
+import contextlib
 import copy
 import logging
 
-import contextlib2
 import tensorflow.compat.v1 as tf
 from xxx import metrics as contrib_metrics
 from tensorflow.contrib import labeled_tensor as lt
@@ -187,6 +187,6 @@ def streaming_std(tensor):
 
 def run_until_exhausted(supervisor, session, fetches):
   """Run the given fetches until OutOfRangeError is triggered."""
-  with contextlib2.suppress(tf.errors.OutOfRangeError):
+  with contextlib.suppress(tf.errors.OutOfRangeError):
     while not supervisor.should_stop():
       yield session.run(fetches)
